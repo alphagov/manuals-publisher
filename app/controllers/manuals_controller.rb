8b28c1ad4727fca343a2095f42acf71e53c658f6
Dominic Baggott <dominic.baggott@gmail.com>
2014-06-17 20:43:46 +0100

    Add ManualServiceRegistry
    A registry containing just the services for manuals is simpler to reason
    about and has fewer dependencies.
    
    By constructing it in the controller we make the codebase easier to work
    with for newcomers whilst keeping the benefits of injecting
    dependencies.

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -1,25 +1,24 @@
 class ManualsController < ApplicationController
   def index
-    all_manuals = services.list_manuals(self).call
+    all_manuals = services.list(self).call
 
     render(:index, locals: { manuals: all_manuals })
   end
 
   def show
-    manual = services.show_manual(self).call
+    manual = services.show(self).call
 
     render(:show, locals: { manual: manual })
   end
 
   def new
-    # manual = services.new_manual(self).call
     manual = nil
 
     render(:new, locals: { manual: manual_form(manual) })
   end
 
   def create
-    manual = services.create_manual(self).call
+    manual = services.create(self).call
     manual = manual_form(manual)
 
     if manual.valid?
@@ -32,13 +31,13 @@ class ManualsController < ApplicationController
   end
 
   def edit
-    manual = services.show_manual(self).call
+    manual = services.show(self).call
 
     render(:edit, locals: { manual: manual_form(manual) })
   end
 
   def update
-    manual = services.update_manual(self).call
+    manual = services.update(self).call
     manual = manual_form(manual)
 
     if manual.valid?
@@ -51,14 +50,38 @@ class ManualsController < ApplicationController
   end
 
   def publish
-    manual = services.publish_manual(self).call
+    manual = services.publish(self).call
 
     redirect_to(manual_path(manual), flash: { notice: "Published #{manual.title}" })
   end
 
 private
-
   def manual_form(manual)
     ManualForm.new(manual)
+  end
+
+  def services
+    @services ||= ManualServiceRegistry.new(
+      manual_builder: manual_builder,
+      manual_repository: manual_repository,
+      observers: observers
+    )
+  end
+
+  def manual_builder
+    # TODO Use ManualBuilder.method(:new) instead?
+    SpecialistPublisherWiring.get(:manual_builder)
+  end
+
+  def manual_repository
+    # TODO Get this from a RepositoryRegistry
+    SpecialistPublisherWiring.get(:manual_repository_factory).call(
+      current_organisation_slug
+    )
+  end
+
+  def observers
+    # TODO Get a set of manual-specific observers
+    SpecialistPublisherWiring.get(:observers)
   end
 end
