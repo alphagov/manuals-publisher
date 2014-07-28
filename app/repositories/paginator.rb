5163a2392619f0039aa882392e9cc82d69869249
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-07-28 11:44:53 +0100

    Adding methods that Kaminari needs for pagination
    Kaminari required some more methods in order to actually paginate.

--- app/repositories/paginator.rb
+++ app/repositories/paginator.rb
@@ -1,6 +1,8 @@
 class RepositoryPaginator
   def initialize(repo)
     @repo = repo
+    @drop = 0
+    @take = 0
     @pipeline = []
   end
 
@@ -8,14 +10,44 @@ class RepositoryPaginator
     apply_pipeline(repo.all(limit, offset))
   end
 
-  def map(*args, &block)
-    pipeline.push([:map, block])
+  def to_a
+    self[@drop, @drop + @take]
+  end
+
+  def drop(n)
+    @drop += n
 
     self
   end
 
+  def take(n)
+    @take = n
+
+    self
+  end
+
+  lazy_methods = %i(
+    chunk
+    map
+    reject
+    select
+    zip
+  )
+
+  lazy_methods.each do |m|
+    define_method(m) do |*args, &block|
+      pipeline.push([m, args, block])
+
+      self
+    end
+  end
+
   def count
     repo.count
+  end
+
+  def size
+    count
   end
 
 private
@@ -23,8 +55,8 @@ private
   attr_reader :repo, :pipeline
 
   def apply_pipeline(results)
-    pipeline.reduce(results) { |collection, (op, func)|
-      collection.public_send(op, &func)
+    pipeline.reduce(results) { |collection, (op, args, func)|
+      collection.public_send(op, *args, &func)
     }
   end
 end
