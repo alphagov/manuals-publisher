ba9d84e1695b6f149b3dcc473600465053fa04a4
Andrew Mitchell <themitchell@me.com>
2014-08-07 14:56:22 +0100

    Change PublishingTask state on error or success
    When something fails in pushing to panopticon let the PanopticonRegisterer raise its own error. This will bubble up to the worker allowing us to rescue from specific error types. In the case of ClientError we should abort as this is unrescuable (ie duplicate slugs). In the case of a ServerError we should retry a certain number of times. In the case of any other error, we should abort.
    
    All errors are logged with Airbrake in order to notify us of persistent failures in publishing.

--- app/workers/publish_manual_worker.rb
+++ app/workers/publish_manual_worker.rb
@@ -1,13 +1,52 @@
 class PublishManualWorker
   include Sidekiq::Worker
 
+  sidekiq_options(
+    retry: 25,
+    backtrace: true,
+  )
+
   def perform(task_id)
     task = ManualPublishTask.find(task_id)
+    task.start!
+
     services.publish(task.manual_id, task.version_number).call
+
+    task.finish!
+  rescue PanopticonRegisterer::ServerError => error
+    requeue_task(task_id, error)
+  rescue PanopticonRegisterer::ClientError => error
+    abort_task(task, error)
+  rescue StandardError => error
+    abort_task(task, error)
+  ensure
+    log_error(error)
   end
 
 private
   def services
     ManualServiceRegistry.new
+  end
+
+  def requeue_task(manual_id, error)
+    raise FailedToPublishError.new("Failed to publish manual with id: #{manual_id}", error)
+  end
+
+  def abort_task(task, error)
+    task.update_attribute(:error, error.message)
+    task.abort!
+  end
+
+  def log_error(error)
+    Airbrake.notify(error)
+  end
+
+  class FailedToPublishError < StandardError
+    attr_reader :original_exception
+
+    def initialize(message, original_exception = nil)
+      super(message)
+      @original_exception = original_exception
+    end
   end
 end
