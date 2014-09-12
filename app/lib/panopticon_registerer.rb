df812e7b0e4bdb0d43831ac82e9d563583e9f845
Andrew Mitchell <themitchell@me.com>
2014-09-12 15:06:26 +0100

    Remove unrequired PanopticonRegisterer errors

--- app/lib/panopticon_registerer.rb
+++ app/lib/panopticon_registerer.rb
@@ -21,32 +21,14 @@ private
       .artefact_for_slug(slug)
       .on_success { |_response| notify_of_update } # TODO Check owning app is "specialist-publisher"
       .on_not_found { |*_| register_new_artefact }
-      .on_error(&method(:handle_error))
   end
 
   def register_new_artefact
-    api
-      .create_artefact!(artefact_attributes)
-      .on_error(&method(:handle_error))
+    api.create_artefact!(artefact_attributes)
   end
 
   def notify_of_update
-    api
-      .put_artefact!(slug, artefact_attributes)
-      .on_error(&method(:handle_error))
-  end
-
-  def handle_error(error, *_api_args)
-    error_logger.call(error)
-
-    case error.code.to_i
-    when (400..499)
-      raise ClientError.new("Panopticon responded with #{error.code}", error)
-    when (500..599)
-      raise ServerError.new("Panopticon responded with #{error.code}", error)
-    else
-      raise HTTPError.new("Panopticon responded with #{error.code}", error)
-    end
+    api.put_artefact!(slug, artefact_attributes)
   end
 
   def slug
@@ -61,20 +43,5 @@ private
 
   def owning_app
     "specialist-publisher"
-  end
-
-  class HTTPError < StandardError
-    attr_reader :original_exception
-
-    def initialize(message, original_exception = nil)
-      super(message)
-      @original_exception = original_exception
-    end
-  end
-
-  class ClientError < HTTPError
-  end
-
-  class ServerError < HTTPError
   end
 end
