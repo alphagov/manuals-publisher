e3b73005408a91d726ce9454ed9477ab85d44315
David Basalla <davidbasalla@gmail.com>
2015-11-24 11:37:20 +0000

    Add script and class to handle manual section reslugging
    We add a script that calls a class for reslugging manual sections. We first withdraw the old section which places a `gone_item` into the content-store. Then we change the slug of the manual section in the local database. Finally we publish the manual which the section belongs to.
    
    The workflow is inspired by https://github.gds/pages/gds/opsmanual/2nd-line/changing-specialist-publisher-document-slug.html
    
    This PR addresses https://trello.com/c/6zdENr8F/93-re-slug-manual-section
    
    Usage:
    `./reslug_manual_section guidance/countryside-stewardship-manual 8-terms-and-conditions 8-scheme-requirements-and-procedures`

---
+++ lib/manual_section_reslugger.rb
@@ -0,0 +1,114 @@
+require "gds_api/content_store"
+require "manual_service_registry"
+
+class ManualSectionReslugger
+  class Error < RuntimeError; end
+
+  def initialize(manual_slug, current_section_slug, new_section_slug)
+    @manual_slug = manual_slug
+    @current_section_slug = current_section_slug
+    @new_section_slug = new_section_slug
+  end
+
+  def call
+    validate
+
+    withdraw_section
+    update_slug
+    publish_manual
+  end
+
+  private
+
+  def validate
+    validate_manual
+    validate_current_section
+    validate_new_section
+  end
+
+  def validate_manual
+    raise Error.new("Manual not found for manual_slug `#{manual_slug}`") if manual.nil?
+  end
+
+  def validate_current_section
+    validate_current_section_in_database
+    validate_current_section_in_content_store
+  end
+
+  def validate_current_section_in_database
+    raise Error.new("Manual Section does not exist in database") if current_section.nil?
+  end
+
+  def validate_current_section_in_content_store
+    raise Error.new("Manual Section does not exist in content store") if current_section_in_content_store.nil?
+    raise Error.new("Manual Section already withdrawn") if current_section_in_content_store.format == "gone"
+  end
+
+  def validate_new_section
+    validate_new_section_in_database
+    validate_new_section_in_content_store
+  end
+
+  def validate_new_section_in_database
+    section = section_in_database(full_new_section_slug)
+    raise Error.new("Manual Section already exists in database") if section
+  end
+
+  def validate_new_section_in_content_store
+    section = section_in_content_store(full_new_section_slug)
+    raise Error.new("Manual Section already exists in content store") if section
+  end
+
+  def withdraw_section
+    PublishingAPIWithdrawer.new(
+      publishing_api: SpecialistPublisherWiring.get(:publishing_api),
+      entity: current_section,
+    ).call
+  end
+
+  def update_slug
+    current_section.update_attribute(:slug, full_new_section_slug)
+    #Manual only republishes documents that have exported_at set to nil
+    current_section.update_attribute(:exported_at, nil)
+  end
+
+  def publish_manual
+    ManualServiceRegistry.new.queue_publish(manual.manual_id).call
+  end
+
+  def manual
+    @manual ||= ManualRecord.where(slug: @manual_slug).last
+  end
+
+  def current_section
+    @current_section ||= section_in_database(full_current_section_slug)
+  end
+
+  def current_section_in_content_store
+    @current_section_in_cs ||= section_in_content_store(full_current_section_slug)
+  end
+
+  def section_in_database(slug)
+    SpecialistDocumentEdition.where(slug: slug).last
+  end
+
+  def section_in_content_store(slug)
+    content_store.content_item("/#{slug}")
+  end
+
+  def content_store
+    GdsApi::ContentStore.new(Plek.current.find("content-store"))
+  end
+
+  def full_current_section_slug
+    full_section_slug(@current_section_slug)
+  end
+
+  def full_new_section_slug
+    full_section_slug(@new_section_slug)
+  end
+
+  def full_section_slug(slug)
+    "#{manual.slug}/#{slug}"
+  end
+end
