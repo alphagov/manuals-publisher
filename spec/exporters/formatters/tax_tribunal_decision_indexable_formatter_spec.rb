207968abddb7c1b4e28eae449f13c6e9bc04188d
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2016-03-18 13:37:03 +0000

    Extract out shared examples for tribunal decision hidden_indexable_content
    Replace the repeated examples for testing hidden_indexable_content with
    the shared examples in the tribunal decision indexable formatter specs.

--- spec/exporters/formatters/tax_tribunal_decision_indexable_formatter_spec.rb
+++ spec/exporters/formatters/tax_tribunal_decision_indexable_formatter_spec.rb
@@ -31,23 +31,6 @@ RSpec.describe TaxTribunalDecisionIndexableFormatter do
     expect(formatter.type).to eq("tax_tribunal_decision")
   end
 
-  context "without hidden_indexable_content" do
-    it "should have body as its indexable_content" do
-      allow(document).to receive(:body).and_return("body text")
-
-      allow(document).to receive(:hidden_indexable_content).and_return(nil)
-      expect(formatter.indexable_attributes[:indexable_content]).to eq("body text")
-    end
-  end
-
-  context "with hidden_indexable_content" do
-    it "should have hidden_indexable_content as its indexable_content" do
-      allow(document).to receive(:body).and_return("body text")
-      allow(document).to receive(:hidden_indexable_content).and_return("hidden indexable content text")
-
-      indexable = formatter.indexable_attributes[:indexable_content]
-      expect(indexable).to eq("hidden indexable content text\nbody text")
-    end
-  end
+  include_examples "tribunal decision hidden_indexable_content"
 
 end
