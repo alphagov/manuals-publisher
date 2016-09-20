9e6b913343a7735b8ff0bc9a5999d9582bc85165
Seraphiana <seraphiana@hotmail.co.uk>
2016-09-20 11:48:54 +0100

    Removed Specialist Document Routes
    Now that specialist documents are handled entirely by Specialist
    Publisher, the old routes have been disabled to facilitate the
    separation of the two apps into Manuals Publisher and Specialist
    Publisher.

--- config/routes.rb
+++ config/routes.rb
@@ -8,28 +8,6 @@ ManualsPublisher::Application.routes.draw do
     mount Sidekiq::Web => "/sidekiq"
   end
 
-  document_types = ManualsPublisher.document_types.map(&:pluralize)
-
-  document_types.each do |type|
-    type_slug = type.to_s.gsub("_", "-")
-
-    resources type.to_sym, controller: type, except: :destroy, path: type_slug do
-      resources :attachments, only: [:new, :create, :edit, :update]
-      post :withdraw, on: :member
-      post :publish, on: :member
-
-      # This is for persisted documents
-      post :preview, on: :member
-    end
-
-    # This is for new documents
-    post "#{type_slug}/preview" => "abstract_documents#preview", as: "preview_new_#{type}"
-  end
-
-  # Redirect old specialist-document routes to cma-cases
-  get "/specialist-documents", to: redirect("/cma-cases")
-  get "/specialist-documents/(*path)", to: redirect { |params, _| "/cma-cases/#{params[:path]}" }
-
   resources :manuals, except: :destroy do
     resources :documents, except: :destroy, path: "sections", controller: "ManualDocuments" do
       resources :attachments, controller: :manual_document_attachments, only: [:new, :create, :edit, :update]
