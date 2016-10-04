3162f0e72fd53bfd1cf87f5661bb198ff785940e
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-10-04 14:27:19 +0000

    Remove specialist document models

--- app/view_adapters/view_adapter_registry.rb
+++ app/view_adapters/view_adapter_registry.rb
@@ -4,23 +4,7 @@ class ViewAdapterRegistry
   end
 
 private
-  VIEW_ADAPTER_MAP = {
-    "aaib_report" => AaibReportViewAdapter,
-    "asylum_support_decision" => AsylumSupportDecisionViewAdapter,
-    "cma_case" => CmaCaseViewAdapter,
-    "countryside_stewardship_grant" => CountrysideStewardshipGrantViewAdapter,
-    "drug_safety_update" => DrugSafetyUpdateViewAdapter,
-    "employment_appeal_tribunal_decision" => EmploymentAppealTribunalDecisionViewAdapter,
-    "employment_tribunal_decision" => EmploymentTribunalDecisionViewAdapter,
-    "esi_fund" => EsiFundViewAdapter,
-    "international_development_fund" => InternationalDevelopmentFundViewAdapter,
-    "maib_report" => MaibReportViewAdapter,
-    "medical_safety_alert" => MedicalSafetyAlertViewAdapter,
-    "raib_report" => RaibReportViewAdapter,
-    "tax_tribunal_decision" => TaxTribunalDecisionViewAdapter,
-    "utaac_decision" => UtaacDecisionViewAdapter,
-    "vehicle_recalls_and_faults_alert" => VehicleRecallsAndFaultsAlertViewAdapter,
-  }.freeze
+  VIEW_ADAPTER_MAP = {}.freeze
 
   def get(type)
     VIEW_ADAPTER_MAP.fetch(type)
