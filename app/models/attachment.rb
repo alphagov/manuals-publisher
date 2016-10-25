61a27a0917badc6bd1448b342e465030246b5e55
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-10-25 17:51:24 +0000

    Extract file upload logic from Attachable
    The govuk_content_models trait "Attachable" is not compatible with
    hash responses from gds-api-adapters. We've recently updated the
    gds-api-adapters gem but this breaks interaction with the asset API.
    So extract this logic into the Attachment class and handle
    hash responses from gds-api-adapters.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,15 +1,15 @@
-require "attachable"
-
 class Attachment
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Attachable
 
   field :title
   field :filename
-  attaches :file, with_url_field: true, update_existing: true
+  field :file_id, type: String
+  field :file_url, type: String
 
   embedded_in :specialist_document_edition
+
+  before_save :upload_file, if: :file_has_changed?
 
   def to_param
     id
@@ -18,4 +18,40 @@ class Attachment
   def snippet
     "[InlineAttachment:#{filename}]"
   end
+
+  def file
+    raise ApiClientNotPresent unless AttachmentApi.client
+    unless file_id.nil?
+      @attachments ||= { }
+      @attachments[field] ||= AttachmentApi.client.asset(file_id)
+    end
+  end
+
+  def file=(file)
+    @file_has_changed = true
+    @uploaded_file = file
+  end
+
+  def file_has_changed?
+    @file_has_changed
+  end
+
+  def upload_file
+    raise ApiClientNotPresent unless AttachmentApi.client
+    begin
+      if file_id.nil?
+        response = AttachmentApi.client.create_asset(file: @uploaded_file)
+        self.file_id = response["id"].split("/").last
+      else
+        response = AttachmentApi.client.update_asset(file_id, file: @uploaded_file)
+      end
+      self.file_url = response["file_url"]
+    rescue StandardError
+      errors.add(:file_id, "could not be uploaded")
+    end
+  end
+
+  private :upload_file
+
+  class ::ApiClientNotPresent < StandardError; end
 end
