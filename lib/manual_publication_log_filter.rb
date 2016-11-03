680bde3aadc062eb1dce2456a88da647a842bae2
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2016-11-03 13:28:23 +0000

    Add class for rebuilding major manual updates
    The existing publication logs for manual edition updates appear to be
    out by a few seconds from the exported_at timestamp on the edition and
    I'm not sure why. In this rebuilding I have assumed that it is acceptable to
    use the exact exported_at timestamp for the publication log.

---
+++ lib/manual_publication_log_filter.rb
@@ -0,0 +1,22 @@
+class ManualPublicationLogFilter
+  def delete_logs_and_rebuild_for_major_updates_only!(slug)
+    PublicationLog.with_slug_prefix(slug).destroy_all
+
+    document_editions_for_rebuild(slug).each do |edition|
+      PublicationLog.create!(
+        title: edition.title,
+        slug: edition.slug,
+        version_number: edition.version_number,
+        change_note: edition.change_note,
+        created_at: edition.exported_at,
+        updated_at: edition.exported_at
+      )
+    end
+  end
+
+  private
+
+  def document_editions_for_rebuild(slug)
+    SpecialistDocumentEdition.with_slug_prefix(slug).where(:minor_update.nin => [true]).any_of({state: "published"}, {state: "archived"})
+  end
+end
