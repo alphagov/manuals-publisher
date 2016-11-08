654ca53b35be6c3b547df1be8f3e2e5518e31ec6
Murray Steele <muz@h-lame.com>
2016-11-08 15:32:24 +0000

    Remove bin script for removing manual sections
    This functionality is now available in the UI so we don't need to have
    the bin script or command line functionality.

--- features/step_definitions/removing_manual_section_steps.rb
+++ features/step_definitions/removing_manual_section_steps.rb
@@ -1,37 +1,3 @@
-When(/^I run the manual section removal script$/) do
-  @stdin = double(:stdin)
-  null_stdout = double(:null_output_io, puts: nil)
-  null_stderr = double(:null_output_io, puts: nil)
-
-  @remover = CliManualSectionRemover.new(
-    manual_id: @manual.id,
-    section_id: @section.id,
-    options: {
-      stdin: @stdin,
-      stdout: null_stdout,
-      stderr: null_stderr,
-    },
-  )
-end
-
-When(/^I confirm removal$/) do
-  allow(@stdin).to receive(:gets).and_return("y")
-  @remover.call
-end
-
-When(/^I refuse removal$/) do
-  allow(@stdin).to receive(:gets).and_return("No")
-  expect { @remover.call }.to raise_error
-end
-
-Then(/^the manual section is removed$/) do
-  check_manual_section_was_removed(@manual.id, @section.id)
-end
-
-Then(/^the manual section still exists$/) do
-  check_manual_section_exists(@manual.id, @section.id)
-end
-
 When(/^I remove the document from the manual$/) do
   withdraw_manual_document(@manual_fields.fetch(:title), @document_fields.fetch(:section_title))
   @removed_document = @document
