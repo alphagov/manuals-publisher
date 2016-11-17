b7072075805bc032544a164efe5a5e4e234477bb
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2016-11-17 14:27:04 +0000

    Create rebuilt manual publication logs in best-guess section order
    The order of creation is important because it ultimately affects the order
    of updates on the front end.
    
    Our best guess of the document order (which can change throughout the lifetime
    of a manual) is to use the order stored on the most recent manual
    edition. Any editions that were previously removed are displayed in created at
    order.
    
    The result should be that the frontend will show updates in best guess
    section order when grouped by day.

--- spec/lib/manual_publication_log_filter_spec.rb
+++ spec/lib/manual_publication_log_filter_spec.rb
@@ -3,16 +3,9 @@ require "manual_publication_log_filter"
 
 describe ManualPublicationLogFilter, "# delete_logs_and_rebuild_for_major_updates_only!" do
   let(:manual_slug) { "guidance/the-highway-code" }
+  let!(:manual_record) { ManualRecord.create(slug: manual_slug) }
   let(:other_slug) { "guidance/sellotape" }
   let(:exported_time) { Time.current }
-
-  let!(:published_major_update_document_edition) do
-    create(:specialist_document_edition,
-           state: "published",
-           slug: "#{manual_slug}/further-info",
-           exported_at: exported_time - 1.day
-          )
-  end
 
   let!(:archived_major_update_document_edition) do
     create(:specialist_document_edition,
@@ -33,8 +26,24 @@ describe ManualPublicationLogFilter, "# delete_logs_and_rebuild_for_major_update
   let!(:published_minor_update_document_edition) do
     create(:specialist_document_edition,
            state: "published",
-           slug: "#{manual_slug}/further-info",
+           slug: "#{manual_slug}/first-further-info",
            minor_update: true
+          )
+  end
+
+  let!(:published_major_update_document_edition_1) do
+    create(:specialist_document_edition,
+           state: "published",
+           slug: "#{manual_slug}/first-further-info",
+           exported_at: exported_time - 1.day
+          )
+  end
+
+  let!(:published_major_update_document_edition_2) do
+    create(:specialist_document_edition,
+           state: "published",
+           slug: "#{manual_slug}/second-further-info",
+           exported_at: exported_time - 1.day
           )
   end
 
@@ -42,6 +51,26 @@ describe ManualPublicationLogFilter, "# delete_logs_and_rebuild_for_major_update
   let!(:previous_other_publication_log) { create :publication_log, slug: other_slug }
 
   before do
+    manual_record.editions.create!(
+      state: "published",
+      version_number: 1,
+      document_ids: [
+        published_major_update_document_edition_1.document_id,
+        archived_major_update_document_edition.document_id,
+        published_major_update_document_edition_2.document_id
+      ]
+    )
+
+    manual_record.editions.create!(
+      state: "published",
+      version_number: 2,
+      document_ids: [
+        published_major_update_document_edition_1.document_id,
+        published_major_update_document_edition_2.document_id,
+        archived_major_update_document_edition.document_id,
+      ]
+    )
+
     subject.delete_logs_and_rebuild_for_major_updates_only!(manual_slug)
   end
 
@@ -52,13 +81,14 @@ describe ManualPublicationLogFilter, "# delete_logs_and_rebuild_for_major_update
     expect(PublicationLog.where(_id: previous_other_publication_log.id).exists?).to eq true
   end
 
-  it "builds logs for major updates in the 'archived' and 'published' status only" do
-    publication_logs_for_supplied_slug = PublicationLog.with_slug_prefix(manual_slug)
+  it "builds logs for major updates in the 'archived' and 'published' status only in the same order as the documents on the most recent manual" do
+    publication_logs_for_supplied_slug = PublicationLog.with_slug_prefix(manual_slug).order_by(:id, :asc)
 
-    expect(publication_logs_for_supplied_slug.count).to eq 2
+    expect(publication_logs_for_supplied_slug.count).to eq 3
 
-    expect_log_attributes_to_match_edition(PublicationLog.where(slug: published_major_update_document_edition.slug).first, published_major_update_document_edition)
-    expect_log_attributes_to_match_edition(PublicationLog.where(slug: archived_major_update_document_edition.slug).first, archived_major_update_document_edition)
+    expect_log_attributes_to_match_edition(publication_logs_for_supplied_slug[0], published_major_update_document_edition_1)
+    expect_log_attributes_to_match_edition(publication_logs_for_supplied_slug[1], published_major_update_document_edition_2)
+    expect_log_attributes_to_match_edition(publication_logs_for_supplied_slug[2], archived_major_update_document_edition)
   end
 
   def expect_log_attributes_to_match_edition(log, edition)
@@ -70,5 +100,36 @@ describe ManualPublicationLogFilter, "# delete_logs_and_rebuild_for_major_update
       created_at: edition.exported_at,
       updated_at: edition.exported_at
     )
+  end
+end
+
+describe ManualPublicationLogFilter::EditionOrdering do
+  describe ".sort_by_document_ids_and_created_at" do
+    let!(:edition_in_third_position) { create :specialist_document_edition }
+    let!(:edition_in_first_position) { create :specialist_document_edition }
+    let!(:edition_in_second_position) { create :specialist_document_edition }
+
+    let!(:other_edition_newer) { create :specialist_document_edition, created_at: Time.now - 1.day }
+    let!(:other_edition_older) { create :specialist_document_edition, created_at: Time.now - 1.week }
+
+    let!(:document_ids) {
+      [
+        edition_in_first_position.document_id,
+        edition_in_second_position.document_id,
+        edition_in_third_position.document_id,
+      ]
+    }
+
+    let(:expected_document_order) {
+      document_ids.concat([other_edition_older.document_id, other_edition_newer.document_id])
+    }
+
+    let(:subject) { described_class.new(SpecialistDocumentEdition.all, document_ids) }
+
+    it "returns editions in the supplied document id and created_at order" do
+      ordered_editions = subject.sort_by_document_ids_and_created_at
+
+      expect(ordered_editions.map(&:document_id)).to eq expected_document_order
+    end
   end
 end
