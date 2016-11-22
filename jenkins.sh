8685703b924c548e6f8b9f8d4a40a911905053c4
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-11-22 15:03:09 +0000

    Change default govuk-content-schemas branch
    Change the branch from master to deployed-to-production. This will help
    prevent changes being merged in to the Manuals Publisher app that don't
    work with the currently deployed govuk-content-schemas (this changes
    comes from GOV.UK RFC 59).

--- jenkins.sh
+++ jenkins.sh
@@ -39,7 +39,7 @@ bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without devel
 
 # Clone govuk-content-schemas depedency for contract tests
 rm -rf tmp/govuk-content-schemas
-git clone git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
+git clone --branch deployed-to-production git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
 
 GOVUK_CONTENT_SCHEMAS_PATH=tmp/govuk-content-schemas RAILS_ENV=test bundle exec rake
 
