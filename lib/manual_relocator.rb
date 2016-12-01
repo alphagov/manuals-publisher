fa64383d9128b81a73b56ba4f17a56222596b219
Murray Steele <muz@h-lame.com>
2016-12-01 10:23:38 +0000

    Issue gone unpublishings for slugs we'll reuse
    To reuse a path in publishing api you have to turn it into one of the
    types that can be substituted.  In our case a gone is the most sensible
    option.  We send a gone for the existing manual base path itself as this
    path will be re-used by the temporary manual when we re-slug it.  We also
    work out if any sections on the existing manual would have the same path
    as any sections on the temporary manual once they are reslugged and issue
    gones for those existing manual sections.

--- lib/manual_relocator.rb
+++ lib/manual_relocator.rb
@@ -32,22 +32,35 @@ private
     manuals.first
   end
 
+  def old_manual_document_ids
+    @old_manual_document_ids ||= old_manual.editions.flat_map(&:document_ids).uniq
+  end
+
+  def new_manual_document_ids
+    @new_manual_document_ids ||= new_manual.editions.flat_map(&:document_ids).uniq
+  end
+
   def redirect_and_remove
     if old_manual.editions.any?
       # Redirect all sections of the manual we're going to remove
       # to prevent dead bookmarked URLs.
-      document_ids = old_manual.editions.flat_map(&:document_ids).uniq
-
-      document_ids.each do |document_id|
-        editions = SpecialistDocumentEdition.where(document_id: document_id)
-        edition = editions.last
+      old_manual_document_ids.each do |document_id|
+        editions = all_editions_of_section(document_id)
+        section_slug = editions.first.slug
 
         begin
-          puts "Redirecting content item '/#{edition.slug}' to '/#{old_manual.slug}'"
-          publishing_api.unpublish(edition.document_id,
-                                   type: "redirect",
-                                   alternative_path: "/#{old_manual.slug}",
-                                   discard_drafts: true)
+          if old_sections_reused_in_new_manual.include? document_id
+            puts "Issuing gone for content item '/#{section_slug}' as it will be reused by a section in '#{new_manual.slug}'"
+            publishing_api.unpublish(document_id,
+                                     type: "gone",
+                                     discard_drafts: true)
+          else
+            puts "Redirecting content item '/#{section_slug}' to '/#{old_manual.slug}'"
+            publishing_api.unpublish(document_id,
+                                     type: "redirect",
+                                     alternative_path: "/#{old_manual.slug}",
+                                     discard_drafts: true)
+          end
         rescue GdsApi::HTTPNotFound
           puts "Content item with content_id #{document_id} not present in the publishing API"
         end
@@ -61,15 +74,45 @@ private
     PublicationLog.change_notes_for(old_manual.slug).each { |log| log.destroy }
 
     # Destroy the manual record
-    puts "Destroying manual #{old_manual._id}"
+    puts "Destroying manual #{old_manual.manual_id}"
     old_manual.destroy
+
+    puts "Issuing gone for #{old_manual.manual_id}"
+    publishing_api.unpublish(old_manual.manual_id,
+                             type: "gone",
+                             discard_drafts: true)
+  end
+
+  def old_sections_reused_in_new_manual
+    @old_sections_reused_in_new_manual ||= _calculate_old_sections_reused_in_new_manual
+  end
+
+  def _calculate_old_sections_reused_in_new_manual
+    old_document_ids_and_section_slugs = old_manual_document_ids.map do |document_id|
+      [document_id, most_recent_edition_of_section(document_id).slug.gsub(to_slug, "")]
+    end
+
+    new_section_slugs = new_manual_document_ids.map do |document_id|
+      most_recent_edition_of_section(document_id).slug.gsub(from_slug, "")
+    end
+
+    old_document_ids_and_section_slugs.
+      select { |_document_id, slug| new_section_slugs.include? slug }.
+      map { |document_id, _slug| document_id }
+  end
+
+  def most_recent_edition_of_section(document_id)
+    all_editions_of_section(document_id).first
+  end
+
+  def all_editions_of_section(document_id)
+    SpecialistDocumentEdition.where(document_id: document_id).order_by([:version_number, :desc])
   end
 
   def reslug
     # Reslug the manual sections
-    document_ids = new_manual.editions.flat_map(&:document_ids).uniq
-    document_ids.each do |document_id|
-      sections = SpecialistDocumentEdition.where(document_id: document_id)
+    new_manual_document_ids.each do |document_id|
+      sections = all_editions_of_section(document_id)
       sections.each do |section|
         reslug_msg = "Reslugging section '#{section.slug}'"
         new_section_slug = section.slug.gsub(from_slug, to_slug)
@@ -89,9 +132,9 @@ private
     end
 
     # Clean up manual sections belonging to the temporary manual path
-    document_ids.each do |document_id|
+    new_manual_document_ids.each do |document_id|
       puts "Redirecting #{document_id} to '/#{to_slug}'"
-      most_recent_edition = SpecialistDocumentEdition.where(document_id: document_id).order_by([:version_number, :desc]).first
+      most_recent_edition = most_recent_edition_of_section(document_id)
       publishing_api.unpublish(document_id,
                                type: "redirect",
                                alternative_path: "/#{most_recent_edition.slug}",
--- spec/lib/manual_relocator_spec.rb
+++ spec/lib/manual_relocator_spec.rb
@@ -49,13 +49,16 @@ describe ManualRelocator do
     let!(:temporary_section_1) { FactoryGirl.create(:specialist_document_edition, slug: "#{temp_slug}/temp_section_1", document_id: "abcdef") }
     let!(:temporary_section_2) { FactoryGirl.create(:specialist_document_edition, slug: "#{temp_slug}/temp_section_2", document_id: "bcdefg") }
 
+    let!(:existing_section_3) { FactoryGirl.create(:specialist_document_edition, slug: "#{existing_slug}/section_3", document_id: "34567") }
+    let!(:temporary_section_3) { FactoryGirl.create(:specialist_document_edition, slug: "#{temp_slug}/section_3", document_id: "cdefgh") }
+
     let!(:existing_publication_log) { FactoryGirl.create(:publication_log, slug: "#{existing_slug}/slug-for-existing-section", change_note: "Hello from #{existing_manual_id}") }
     let!(:temporary_publication_log) { FactoryGirl.create(:publication_log, slug: "#{temp_slug}/slug-for-temp-section", change_note: "Hello from #{temp_manual_id}") }
 
     before do
       allow(STDOUT).to receive(:puts)
-      existing_manual.editions << ManualRecord::Edition.new(document_ids: %w(12345 23456))
-      temp_manual.editions << ManualRecord::Edition.new(document_ids: %w(abcdef bcdefg))
+      existing_manual.editions << ManualRecord::Edition.new(document_ids: %w(12345 23456 34567))
+      temp_manual.editions << ManualRecord::Edition.new(document_ids: %w(abcdef bcdefg cdefgh))
       stub_any_publishing_api_unpublish
       subject.move!
     end
@@ -78,6 +81,12 @@ describe ManualRelocator do
                                       discard_drafts: true)
     end
 
+    it "unpublishes the existing manual with a gone" do
+      assert_publishing_api_unpublish(existing_manual_id,
+                                      type: "gone",
+                                      discard_drafts: true)
+    end
+
     it "unpublishes the existing manual's sections with redirects to the existing slug" do
       assert_publishing_api_unpublish(existing_section_1.document_id,
                                       type: "redirect",
@@ -90,6 +99,12 @@ describe ManualRelocator do
                                       discard_drafts: true)
     end
 
+    it "issues a gone for existing manual's sections that would be reused one of the new manual's sections" do
+      assert_publishing_api_unpublish(existing_section_3.document_id,
+                                      type: "gone",
+                                      discard_drafts: true)
+    end
+
     it "destroys the existing manual's sections" do
       expect {
         existing_section_1.reload
@@ -98,11 +113,16 @@ describe ManualRelocator do
       expect {
         existing_section_2.reload
       }.to raise_error(Mongoid::Errors::DocumentNotFound)
+
+      expect {
+        existing_section_3.reload
+      }.to raise_error(Mongoid::Errors::DocumentNotFound)
     end
 
     it "moves the temporary manual's sections to the existing slug" do
       expect(temporary_section_1.reload.slug).to eq("#{existing_slug}/temp_section_1")
       expect(temporary_section_2.reload.slug).to eq("#{existing_slug}/temp_section_2")
+      expect(temporary_section_3.reload.slug).to eq("#{existing_slug}/section_3")
       expect(SpecialistDocumentEdition.where(slug: /#{temp_slug}/).count).to be(0)
     end
 
@@ -115,6 +135,11 @@ describe ManualRelocator do
       assert_publishing_api_unpublish(temporary_section_2.document_id,
                                       type: "redirect",
                                       alternative_path: "/#{existing_slug}/temp_section_2",
+                                      discard_drafts: true)
+
+      assert_publishing_api_unpublish(temporary_section_3.document_id,
+                                      type: "redirect",
+                                      alternative_path: "/#{existing_slug}/section_3",
                                       discard_drafts: true)
     end
 
