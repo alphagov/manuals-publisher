c9ccde384a31f3d999961347410c5271357c71ad
Murray Steele <muz@h-lame.com>
2016-12-07 16:52:33 +0000

    Remove the bad draft in our migration
    As well as removing the old capital-funding-guide manual and
    reslugging the new one, we also want to remove an bad draft
    that has no title.  We use the existing CliManualDeleter for
    this purpose, and pass in a StringIO with a "y" in it to get
    around the required keyboard input.

--- db/migrate/20161031154415_remove_redirect_capital_funding_guide.rb
+++ db/migrate/20161031154415_remove_redirect_capital_funding_guide.rb
@@ -1,9 +1,14 @@
 require "manual_relocator"
+require "cli_manual_deleter"
+require "stringio"
 
 class RemoveRedirectCapitalFundingGuide < Mongoid::Migration
   def self.up
+    bad_manual_id = "7228d773-a416-4ec1-9549-5fb91b1caaf5"
     manual_slug = "guidance/capital-funding-guide"
     new_manual_slug = "#{manual_slug}-hca"
+
+    CliManualDeleter.new(manual_slug, manual_id: bad_manual_id, stdin: StringIO.new("y")).call
 
     ManualRelocator.move(new_manual_slug, manual_slug)
   end
