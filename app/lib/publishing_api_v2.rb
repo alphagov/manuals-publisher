ca0613606ed28b1e3d0119738e71e62d669d8f0e
Chris Roos <chris@seagul.co.uk>
2017-02-01 11:32:09 +0000

    Add PublishingApiV2

---
+++ app/lib/publishing_api_v2.rb
@@ -0,0 +1,10 @@
+require "gds_api/publishing_api_v2"
+
+class PublishingApiV2
+  def self.instance
+    GdsApi::PublishingApiV2.new(
+      Plek.new.find("publishing-api"),
+      bearer_token: ENV["PUBLISHING_API_BEARER_TOKEN"] || "example"
+    )
+  end
+end
