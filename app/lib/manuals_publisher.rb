59197f12ab236fea11b0091f4ce611faf723ddcf
Chris Roos <chris@seagul.co.uk>
2017-02-01 12:15:59 +0000

    Explicitly define builder in ManualsPublisher.document_services
    The `:manual_builder` and `:manual_document_builder` are the only
    entries in `ManualsPublisherWiring.get` whose name ends in `_builder`.
    Making this explicit should help me remove the `ManualsPublisherWiring`
    in future.

--- app/lib/manuals_publisher.rb
+++ app/lib/manuals_publisher.rb
@@ -2,9 +2,15 @@ module ManualsPublisher
   extend self
 
   def document_services(document_type)
+    builder = case document_type
+              when "manual"
+                ManualsPublisherWiring.get(:manual_builder)
+              when "manual_document"
+                ManualsPublisherWiring.get(:manual_document_builder)
+              end
     AbstractDocumentServiceRegistry.new(
       repository: document_repositories.for_type(document_type),
-      builder: ManualsPublisherWiring.get("#{document_type}_builder".to_sym),
+      builder: builder,
       observers: observer_registry(document_type),
     )
   end
