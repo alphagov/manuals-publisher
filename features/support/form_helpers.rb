a793e948fb5b7ef558e99400e28aad896dd7e871
Murray Steele <muz@h-lame.com>
2017-02-01 10:56:50 +0000

    Simplify UI around editing original publication date
    1. Change to "first published at"
    2. Drop the first set of radio buttons that ask the user to choose
       "This manual is new" vs "This manual was first published elsewhere"
    3. Change wording of the second set of radio buttons to simplify the
       choice the users have to make.

--- features/support/form_helpers.rb
+++ features/support/form_helpers.rb
@@ -14,5 +14,16 @@ module FormHelpers
       fill_in label_text, with: value
     end
   end
+
+  def clear_datetime(label)
+    base_dom_id = find(:xpath, ".//label[contains(., '#{label}')]")["for"].gsub(/(_[1-5]i)$/, "")
+
+    find(:xpath, ".//select[@id='#{base_dom_id}_1i']").select("")
+    find(:xpath, ".//select[@id='#{base_dom_id}_2i']").select("")
+    find(:xpath, ".//select[@id='#{base_dom_id}_3i']").select("")
+
+    find(:xpath, ".//select[@id='#{base_dom_id}_4i']").select("")
+    find(:xpath, ".//select[@id='#{base_dom_id}_5i']").select("")
+  end
 end
 RSpec.configuration.include FormHelpers, type: :feature
