24c6fe7f26859c5e5c464f3b527323dc01a1a1d5
Chris Roos <chris@seagul.co.uk>
2017-02-01 11:37:29 +0000

    Add OrganisationFetcher

---
+++ app/lib/organisation_fetcher.rb
@@ -0,0 +1,8 @@
+class OrganisationFetcher
+  def self.instance
+    organisations = {}
+    ->(organisation_slug) {
+      organisations[organisation_slug] ||= OrganisationsApi.instance.organisation(organisation_slug)
+    }
+  end
+end
