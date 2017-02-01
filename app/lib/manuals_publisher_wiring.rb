a5e25035e0529f7b6dbd2bc6343f1fbefdb0036a
Chris Roos <chris@seagul.co.uk>
2017-02-01 11:15:55 +0000

    Add ManualDocumentRenderer

--- app/lib/manuals_publisher_wiring.rb
+++ app/lib/manuals_publisher_wiring.rb
@@ -20,18 +20,7 @@ $LOAD_PATH.unshift(File.expand_path("../..", "app/services"))
 # rubocop:disable ConstantName
 ManualsPublisherWiring ||= DependencyContainer.new do
   define_instance(:manual_document_renderer) {
-    ->(doc) {
-      pipeline = [
-        MarkdownAttachmentProcessor.method(:new),
-        SpecialistDocumentHeaderExtractor.create,
-        GovspeakToHTMLRenderer.create,
-        FootnotesSectionHeadingRenderer.create,
-      ]
-
-      pipeline.reduce(doc) { |doc, next_renderer|
-        next_renderer.call(doc)
-      }
-    }
+    ManualDocumentRenderer.create
   }
 
   define_singleton(:rummager_api) {
