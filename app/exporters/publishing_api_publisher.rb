c526f21b105c8ca60fb643bc41bcc2a2b698316c
Murray Steele <muz@h-lame.com>
2017-02-02 16:09:39 +0000

    Allow setting update_type on PublishingAPIPublisher
    This lets us explicitly tell the system what sort of update we are
    publishing and override what the publishing-api might detect based on
    the data it has about the content item we've asked it to publish.
    
    This'll be handy for issuing republish events.

--- app/exporters/publishing_api_publisher.rb
+++ app/exporters/publishing_api_publisher.rb
@@ -1,11 +1,15 @@
 class PublishingAPIPublisher
-  def initialize(publishing_api:, entity:)
+  UPDATE_TYPES = %w(minor major republish).freeze
+
+  def initialize(publishing_api:, entity:, update_type: nil)
     @publishing_api = publishing_api
     @entity = entity
+    @update_type = update_type
+    raise ArgumentError, "update_type '#{update_type}' not recognised" if update_type.present? && !UPDATE_TYPES.include?(update_type)
   end
 
   def call
-    publishing_api.publish(entity.id, nil)
+    publishing_api.publish(entity.id, update_type)
   end
 
 private
@@ -13,5 +17,6 @@ private
   attr_reader(
     :publishing_api,
     :entity,
+    :update_type,
   )
 end
