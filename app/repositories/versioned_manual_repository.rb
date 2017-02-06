17072977b9a4c893d7e1d6bf2dbfcd2b0d9ed082
Murray Steele <muz@h-lame.com>
2017-02-06 11:28:16 +0000

    Add VersionedManualRepository
    This repo doesn't have the same interface as the existing repositories,
    but as it is intended to be used in a different way I don't think that's
    necessarily a bad thing.
    
    This repo, given a manual id will return a hash containing a published
    key and a draft key.  Either (or both) of these could be nil, although
    the repo will throw and exception if the manual id doesn't refer to an
    actual manual.
    
    The simple case is for a manual that has yet to be published - the
    published key will be empty, and the draft key will contain the Manual
    instance that represents the supplied id, with the most recent version
    of all its specialist documents attached.  If the manual has been
    published, but has no draft its equally simple - the draft key is empty
    and the published key contains the Manual instance.  In the former case
    everything should be in "draft" state, in the latter everything should
    be in "published" state.
    
    It gets complex when the manual has been published and has a new draft
    ready.  In this case both keys are filled.  The draft key has the most
    recent version of the manual and its documents are the most recent
    version of each.  The state of the manual will be "draft" but the state
    of the documents could be "draft" or "published" (depending on if they
    have been edited since the manual was published or not).  The published
    key has a Manual instance that represents the previous version of the
    manual and its documents are the most recent published version of each.
    The state of the manual and all its documents should be "published" in
    this case.
    
    If the current or previous version of the manual is "withdrawn" it is
    not included in the returned hash as either a "draft" or "published"
    version.
    
    Note that because the data model is lossly, we can't correctly obtain
    arbitrary versions of a manual and the correct versions of its documents.
    The data model maintains a link between a manual edition and the document
    editions, but it does not maintain a version link.  All we can say is
    that version N of manual X has document A in it, but not which version of
    document A.  This means we can only say with confidence what versions are
    in the first edition of a manual, the current edition of a manual, and if
    the current edition is draft the previous edition of the manual.
    
    Also worth noting is that the Manual and specialistDocument instances
    returned by this repo are "read only" in that they don't have functioning
    "document_builder" or "slug_generator" factories.  For our purposes this
    is ok (and easier than wading through the abstraction to instantiate the
    correct instances).

---
+++ app/repositories/versioned_manual_repository.rb
@@ -0,0 +1,111 @@
+require "manual_repository"
+
+class VersionedManualRepository
+
+  class NotFoundError < StandardError; include ManualRepository::NotFoundError; end
+
+  def self.get_manual(manual_id)
+    new.get_manual(manual_id)
+  end
+
+  def get_manual(manual_id)
+    manual_record = ManualRecord.find_by(manual_id: manual_id)
+    raise NotFoundError if manual_record.nil?
+
+    {
+      draft: get_current_draft_version_of_manual(manual_record),
+      published: get_current_published_version_of_manual(manual_record),
+    }
+  end
+
+private
+
+  def get_current_draft_version_of_manual(manual_record)
+    return nil unless manual_record.latest_edition.state == "draft"
+
+    build_manual_for(manual_record, manual_record.latest_edition) do
+      {
+        documents: get_latest_version_of_documents(manual_record.latest_edition.document_ids),
+        removed_documents: get_latest_version_of_documents(manual_record.latest_edition.removed_document_ids),
+      }
+    end
+  end
+
+  def get_current_published_version_of_manual(manual_record)
+    return nil unless manual_record.has_ever_been_published?
+
+    if manual_record.latest_edition.state == "published"
+      build_manual_for(manual_record, manual_record.latest_edition) do
+        {
+          documents: get_latest_version_of_documents(manual_record.latest_edition.document_ids),
+          removed_documents: get_latest_version_of_documents(manual_record.latest_edition.removed_document_ids),
+        }
+      end
+    elsif manual_record.latest_edition.state == "draft"
+      previous_edition = manual_record.editions.order_by([:version_number, :desc]).limit(2).last
+      if previous_edition.state == "published"
+        build_manual_for(manual_record, previous_edition) do
+          {
+            documents: get_published_version_of_documents(previous_edition.document_ids),
+            removed_documents: get_latest_version_of_documents(previous_edition.removed_document_ids)
+          }
+        end
+      else
+        # This means the previous edition is withdrawn so we shouldn't
+        # expose it as it's not actually published (we've got a new
+        # draft waiting in the wings for a withdrawn manual)
+        return nil
+      end
+    else
+      # This means the current edition is withdrawn so we shouldn't find
+      # the previously published one
+      return nil
+    end
+  end
+
+  def build_manual_for(manual_record, edition, &document_fetcher_block)
+    base_manual = Manual.new(
+      id: manual_record.manual_id,
+      slug: manual_record.slug,
+      title: edition.title,
+      summary: edition.summary,
+      body: edition.body,
+      organisation_slug: manual_record.organisation_slug,
+      state: edition.state,
+      version_number: edition.version_number,
+      updated_at: edition.updated_at,
+      ever_been_published: manual_record.has_ever_been_published?,
+      originally_published_at: edition.originally_published_at,
+      use_originally_published_at_for_public_timestamp: edition.use_originally_published_at_for_public_timestamp,
+    )
+
+    document_attrs = document_fetcher_block.call
+
+    ManualWithDocuments.new(
+      ->(_manual, _attrs) { raise RuntimeError, "read only manaul" },
+      base_manual,
+      document_attrs
+    )
+  end
+
+  def get_latest_version_of_documents(document_ids)
+    (document_ids || []).map do |document_id|
+      build_specialist_document(document_id) { |editions| editions }
+    end
+  end
+
+  def build_specialist_document(document_id, &filter)
+    all_editions = SpecialistDocumentEdition.where(document_type: "manual", document_id: document_id).order_by([:version_number, :desc]).to_a
+    SpecialistDocument.new(
+      ->(_title) { raise RuntimeError, "read only manual" },
+      document_id,
+      filter.call(all_editions).take(2).reverse,
+    )
+  end
+
+  def get_published_version_of_documents(document_ids)
+    (document_ids || []).map do |document_id|
+      build_specialist_document(document_id) { |editions| editions.drop_while { |e| e.state != "published" } }
+    end
+  end
+end
---
+++ spec/repositories/versioned_manual_repository_spec.rb
@@ -0,0 +1,353 @@
+require "spec_helper"
+
+RSpec.describe VersionedManualRepository do
+  subject(:repository) { described_class.new }
+
+  context "when the provided id doesn't refer to a manual" do
+    it "raises a Not Found error" do
+      expect { subject.get_manual("i-dont-exist") }.to raise_error(ManualRepository::NotFoundError)
+    end
+  end
+
+  context "when the provided id refers to the first draft of a manual" do
+    let(:manual_id) { SecureRandom.uuid }
+    let(:manual) { ManualRecord.create(manual_id: manual_id, slug: "guidance/my-amazing-manual", organisation_slug: "cabinet-office") }
+    let(:manual_edition) { ManualRecord::Edition.new(document_ids: %w(12345 67890), version_number: 1, state: "draft") }
+    let!(:section_1) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-1", document_id: "12345", version_number: 1, state: "draft") }
+    let!(:section_2) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 1, state: "draft") }
+    before do
+      manual.editions << manual_edition
+    end
+
+    context "the published version returned" do
+      subject { repository.get_manual(manual_id)[:published] }
+
+      it "is blank" do
+        expect(subject).to be_nil
+      end
+    end
+
+    context "the draft version returned" do
+      subject { repository.get_manual(manual_id)[:draft] }
+
+      it "is the first draft as a ManualWithDocuments instance" do
+        expect(subject).to be_a ::ManualWithDocuments
+        expect(subject.id).to eq manual_id
+        expect(subject.state).to eq "draft"
+        expect(subject.version_number).to eq 1
+        expect(subject.slug).to eq "guidance/my-amazing-manual"
+      end
+
+      it "has the first draft of the specialist document editions as Specialist Document instances attached" do
+        documents = subject.documents.to_a
+        expect(documents.size).to eq 2
+
+        document_1 = documents[0]
+        expect(document_1).to be_a ::SpecialistDocument
+        expect(document_1.id).to eq "12345"
+        expect(document_1).to be_draft
+        expect(document_1.version_number).to eq 1
+        expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+        document_2 = documents[1]
+        expect(document_2).to be_a ::SpecialistDocument
+        expect(document_2.id).to eq "67890"
+        expect(document_2).to be_draft
+        expect(document_2.version_number).to eq 1
+        expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+      end
+    end
+  end
+
+  context "when the provided id refers to manual that has been published once" do
+    let(:manual_id) { SecureRandom.uuid }
+    let(:manual) { ManualRecord.create(manual_id: manual_id, slug: "guidance/my-amazing-manual", organisation_slug: "cabinet-office") }
+    let(:manual_edition) { ManualRecord::Edition.new(document_ids: %w(12345 67890), version_number: 1, state: "published") }
+    let!(:section_1) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-1", document_id: "12345", version_number: 1, state: "published") }
+    let!(:section_2) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 1, state: "published") }
+    before do
+      manual.editions << manual_edition
+    end
+
+    context "the published version returned" do
+      subject { repository.get_manual(manual_id)[:published] }
+
+      it "is the published version as a ManualWithDocuments instance" do
+        expect(subject).to be_a ::ManualWithDocuments
+        expect(subject.id).to eq manual_id
+        expect(subject.state).to eq "published"
+        expect(subject.version_number).to eq 1
+        expect(subject.slug).to eq "guidance/my-amazing-manual"
+      end
+
+      it "has the published version of the specialist document editions as Specialist Document instances attached" do
+        documents = subject.documents.to_a
+        expect(documents.size).to eq 2
+
+        document_1 = documents[0]
+        expect(document_1).to be_a ::SpecialistDocument
+        expect(document_1.id).to eq "12345"
+        expect(document_1).to be_published
+        expect(document_1.version_number).to eq 1
+        expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+        document_2 = documents[1]
+        expect(document_2).to be_a ::SpecialistDocument
+        expect(document_2.id).to eq "67890"
+        expect(document_2).to be_published
+        expect(document_2.version_number).to eq 1
+        expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+      end
+    end
+
+    context "the draft version returned" do
+      subject { repository.get_manual(manual_id)[:draft] }
+
+      it "is blank" do
+        expect(subject).to be_nil
+      end
+    end
+  end
+
+  context "when the provided id refers to manual that has been withdrawn once" do
+    let(:manual_id) { SecureRandom.uuid }
+    let(:manual) { ManualRecord.create(manual_id: manual_id, slug: "guidance/my-amazing-manual", organisation_slug: "cabinet-office") }
+    let(:manual_edition) { ManualRecord::Edition.new(document_ids: %w(12345 67890), version_number: 1, state: "withdrawn") }
+    let!(:section_1) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-1", document_id: "12345", version_number: 1, state: "archived") }
+    let!(:section_2) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 1, state: "archived") }
+    before do
+      manual.editions << manual_edition
+    end
+
+    context "the published version returned" do
+      subject { repository.get_manual(manual_id)[:published] }
+
+      it "is blank" do
+        expect(subject).to be_nil
+      end
+    end
+
+    context "the draft version returned" do
+      subject { repository.get_manual(manual_id)[:draft] }
+
+      it "is blank" do
+        expect(subject).to be_nil
+      end
+    end
+  end
+
+  context "when the provided id refers to manual that has been published once and has a new draft waiting" do
+    let(:manual_id) { SecureRandom.uuid }
+    let(:manual) { ManualRecord.create(manual_id: manual_id, slug: "guidance/my-amazing-manual", organisation_slug: "cabinet-office") }
+    let(:manual_published_edition) { ManualRecord::Edition.new(document_ids: %w(12345 67890), version_number: 1, state: "published") }
+    let(:manual_draft_edition) { ManualRecord::Edition.new(document_ids: %w(12345 67890), version_number: 2, state: "draft") }
+    before do
+      manual.editions << manual_published_edition
+      manual.editions << manual_draft_edition
+    end
+
+    context "including new drafts of all sections" do
+      let!(:section_1_published) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-1", document_id: "12345", version_number: 1, state: "published") }
+      let!(:section_2_published) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 1, state: "published") }
+      let!(:section_1_draft) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-1", document_id: "12345", version_number: 2, state: "draft") }
+      let!(:section_2_draft) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 2, state: "draft") }
+
+      context "the published version returned" do
+        subject { repository.get_manual(manual_id)[:published] }
+
+        it "is the published version as a ManualWithDocuments instance" do
+          expect(subject).to be_a ::ManualWithDocuments
+          expect(subject.id).to eq manual_id
+          expect(subject.state).to eq "published"
+          expect(subject.version_number).to eq 1
+          expect(subject.slug).to eq "guidance/my-amazing-manual"
+        end
+
+        it "has the published versions of the specialist document editions as Specialist Document instances attached" do
+          documents = subject.documents.to_a
+          expect(documents.size).to eq 2
+
+          document_1 = documents[0]
+          expect(document_1).to be_a ::SpecialistDocument
+          expect(document_1.id).to eq "12345"
+          expect(document_1).to be_published
+          expect(document_1.version_number).to eq 1
+          expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+          document_2 = documents[1]
+          expect(document_2).to be_a ::SpecialistDocument
+          expect(document_2.id).to eq "67890"
+          expect(document_2).to be_published
+          expect(document_2.version_number).to eq 1
+          expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+        end
+      end
+
+      context "the draft version returned" do
+        subject { repository.get_manual(manual_id)[:draft] }
+
+        it "is the new draft as a ManualWithDocuments instance" do
+          expect(subject).to be_a ::ManualWithDocuments
+          expect(subject.id).to eq manual_id
+          expect(subject.state).to eq "draft"
+          expect(subject.version_number).to eq 2
+          expect(subject.slug).to eq "guidance/my-amazing-manual"
+        end
+
+        it "has the new drafts of the specialist document editions as Specialist Document instances attached" do
+          documents = subject.documents.to_a
+          expect(documents.size).to eq 2
+
+          document_1 = documents[0]
+          expect(document_1).to be_a ::SpecialistDocument
+          expect(document_1.id).to eq "12345"
+          expect(document_1).to be_draft
+          expect(document_1.version_number).to eq 2
+          expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+          document_2 = documents[1]
+          expect(document_2).to be_a ::SpecialistDocument
+          expect(document_2.id).to eq "67890"
+          expect(document_2).to be_draft
+          expect(document_2.version_number).to eq 2
+          expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+        end
+      end
+    end
+
+    context "without new drafts of any sections" do
+      let!(:section_1_published) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-1", document_id: "12345", version_number: 1, state: "published") }
+      let!(:section_2_published) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 1, state: "published") }
+
+      context "the published version returned" do
+        subject { repository.get_manual(manual_id)[:published] }
+
+        it "is the published version as a ManualWithDocuments instance" do
+          expect(subject).to be_a ::ManualWithDocuments
+          expect(subject.id).to eq manual_id
+          expect(subject.state).to eq "published"
+          expect(subject.version_number).to eq 1
+          expect(subject.slug).to eq "guidance/my-amazing-manual"
+        end
+
+        it "has the published versions of the specialist document editions as Specialist Document instances attached" do
+          documents = subject.documents.to_a
+          expect(documents.size).to eq 2
+
+          document_1 = documents[0]
+          expect(document_1).to be_a ::SpecialistDocument
+          expect(document_1.id).to eq "12345"
+          expect(document_1).to be_published
+          expect(document_1.version_number).to eq 1
+          expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+          document_2 = documents[1]
+          expect(document_2).to be_a ::SpecialistDocument
+          expect(document_2.id).to eq "67890"
+          expect(document_2).to be_published
+          expect(document_2.version_number).to eq 1
+          expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+        end
+      end
+
+      context "the draft version returned" do
+        subject { repository.get_manual(manual_id)[:draft] }
+
+        it "is the new draft as a ManualWithDocuments instance" do
+          expect(subject).to be_a ::ManualWithDocuments
+          expect(subject.id).to eq manual_id
+          expect(subject.state).to eq "draft"
+          expect(subject.version_number).to eq 2
+          expect(subject.slug).to eq "guidance/my-amazing-manual"
+        end
+
+        it "has the published versions of the specialist document editions as Specialist Document instances attached" do
+          documents = subject.documents.to_a
+          expect(documents.size).to eq 2
+
+          document_1 = documents[0]
+          expect(document_1).to be_a ::SpecialistDocument
+          expect(document_1.id).to eq "12345"
+          expect(document_1).to be_published
+          expect(document_1.version_number).to eq 1
+          expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+          document_2 = documents[1]
+          expect(document_2).to be_a ::SpecialistDocument
+          expect(document_2.id).to eq "67890"
+          expect(document_2).to be_published
+          expect(document_2.version_number).to eq 1
+          expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+        end
+      end
+    end
+
+    context "including new drafts of some sections" do
+      let!(:section_1_published) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-1", document_id: "12345", version_number: 1, state: "published") }
+      let!(:section_2_published) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 1, state: "published") }
+      let!(:section_2_draft) { FactoryGirl.create(:specialist_document_edition, slug: "#{manual.slug}/section-2", document_id: "67890", version_number: 2, state: "draft") }
+
+      context "the published version returned" do
+        subject { repository.get_manual(manual_id)[:published] }
+
+        it "is the published version as a ManualWithDocuments instance" do
+          expect(subject).to be_a ::ManualWithDocuments
+          expect(subject.id).to eq manual_id
+          expect(subject.state).to eq "published"
+          expect(subject.version_number).to eq 1
+          expect(subject.slug).to eq "guidance/my-amazing-manual"
+        end
+
+        it "has the published versions of the specialist document editions as Specialist Document instances attached" do
+          documents = subject.documents.to_a
+          expect(documents.size).to eq 2
+
+          document_1 = documents[0]
+          expect(document_1).to be_a ::SpecialistDocument
+          expect(document_1.id).to eq "12345"
+          expect(document_1).to be_published
+          expect(document_1.version_number).to eq 1
+          expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+          document_2 = documents[1]
+          expect(document_2).to be_a ::SpecialistDocument
+          expect(document_2.id).to eq "67890"
+          expect(document_2).to be_published
+          expect(document_2.version_number).to eq 1
+          expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+        end
+      end
+
+      context "the draft version returned" do
+        subject { repository.get_manual(manual_id)[:draft] }
+
+        it "is the new draft as a ManualWithDocuments instance" do
+          expect(subject).to be_a ::ManualWithDocuments
+          expect(subject.id).to eq manual_id
+          expect(subject.state).to eq "draft"
+          expect(subject.version_number).to eq 2
+          expect(subject.slug).to eq "guidance/my-amazing-manual"
+        end
+
+        it "has correct draft or published version of the specialist document editions as Specialist Document instances attached" do
+          documents = subject.documents.to_a
+          expect(documents.size).to eq 2
+
+          document_1 = documents[0]
+          expect(document_1).to be_a ::SpecialistDocument
+          expect(document_1.id).to eq "12345"
+          expect(document_1).to be_published
+          expect(document_1.version_number).to eq 1
+          expect(document_1.slug).to eq "guidance/my-amazing-manual/section-1"
+
+          document_2 = documents[1]
+          expect(document_2).to be_a ::SpecialistDocument
+          expect(document_2.id).to eq "67890"
+          expect(document_2).to be_draft
+          expect(document_2.version_number).to eq 2
+          expect(document_2.slug).to eq "guidance/my-amazing-manual/section-2"
+        end
+      end
+    end
+  end
+end
