6110a9597b1dfcf4b5f64a1e8eab584d896c4c5f
James Mead <james@floehopper.org>
2017-03-09 11:51:53 +0000

    Remove unused PublishingApi.instance method

--- app/lib/publishing_api.rb
+++ app/lib/publishing_api.rb
@@ -1,11 +1,2 @@
-require "gds_api/publishing_api"
-
 class PublishingApi
-  def self.instance
-    @publishing_api ||= GdsApi::PublishingApi.new(
-      Plek.new.find("publishing-api"),
-      bearer_token: ENV["PUBLISHING_API_BEARER_TOKEN"] || "example",
-      timeout: 30
-    )
-  end
 end
