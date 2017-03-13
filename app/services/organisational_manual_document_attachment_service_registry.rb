708f423c15af72f2b6fdf88bda5e19c9ccffc168
James Mead <james@floehopper.org>
2017-03-13 17:05:40 +0000

    Inline manual_repository_factory method on service registry

--- app/services/organisational_manual_document_attachment_service_registry.rb
+++ app/services/organisational_manual_document_attachment_service_registry.rb
@@ -4,15 +4,12 @@ class OrganisationalManualDocumentAttachmentServiceRegistry < AbstractManualDocu
   end
 
   def repository
+    manual_repository_factory = RepositoryRegistry.create
+      .organisation_scoped_manual_repository_factory
     manual_repository_factory.call(organisation_slug)
   end
 
 private
 
   attr_reader :organisation_slug
-
-  def manual_repository_factory
-    RepositoryRegistry.create
-      .organisation_scoped_manual_repository_factory
-  end
 end
