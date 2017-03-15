283241ca92b535e679776d14415e96d5ae1eea22
James Mead <james@floehopper.org>
2017-03-15 08:45:58 +0000

    Remove unused service registry organisation_slug attribute

--- app/services/organisational_section_service_registry.rb
+++ app/services/organisational_section_service_registry.rb
@@ -2,6 +2,4 @@ class OrganisationalSectionServiceRegistry < AbstractSectionServiceRegistry
   def initialize(organisation_slug:)
     @organisation_slug = organisation_slug
   end
-
-  attr_reader :organisation_slug
 end
