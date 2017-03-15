0699ce452865faeea2a0a961bef47c4cb5f9ec1f
James Mead <james@floehopper.org>
2017-03-15 14:26:56 +0000

    Inline org service registry #manual_repository_factory

--- app/services/organisational_manual_service_registry.rb
+++ app/services/organisational_manual_service_registry.rb
@@ -6,16 +6,13 @@ class OrganisationalManualServiceRegistry < AbstractManualServiceRegistry
   attr_reader :organisation_slug
 
   def repository
+    manual_repository_factory = RepositoryRegistry.create
+      .organisation_scoped_manual_repository_factory
     manual_repository_factory.call(organisation_slug)
   end
 
   def associationless_repository
     associationless_manual_repository_factory.call(organisation_slug)
-  end
-
-  def manual_repository_factory
-    RepositoryRegistry.create
-      .organisation_scoped_manual_repository_factory
   end
 
   def associationless_manual_repository_factory
