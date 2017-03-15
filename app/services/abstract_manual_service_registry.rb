fd72ded555303af7db60bf779025c5f6cad3c4f8
James Mead <james@floehopper.org>
2017-03-15 13:48:15 +0000

    Inline AbstractManualServiceRegistry#republish

--- app/services/abstract_manual_service_registry.rb
+++ app/services/abstract_manual_service_registry.rb
@@ -1,17 +1,8 @@
-require "republish_manual_service"
 require "withdraw_manual_service"
 require "manual_observers_registry"
 require "builders/manual_builder"
 
 class AbstractManualServiceRegistry
-  def republish(manual_id)
-    RepublishManualService.new(
-      draft_listeners: observers.update,
-      published_listeners: observers.republication,
-      manual_id: manual_id,
-    )
-  end
-
   def withdraw(manual_id)
     WithdrawManualService.new(
       manual_repository: repository,
--- lib/manuals_republisher.rb
+++ lib/manuals_republisher.rb
@@ -17,7 +17,13 @@ class ManualsRepublisher
     manual_records.to_a.each.with_index do |manual_record, i|
       begin
         logger.info("[ #{i} / #{count} ] id=#{manual_record.manual_id} slug=#{manual_record.slug}]")
-        ManualServiceRegistry.new.republish(manual_record.manual_id).call
+        services = ManualServiceRegistry.new
+        service = RepublishManualService.new(
+          draft_listeners: services.observers.update,
+          published_listeners: services.observers.republication,
+          manual_id: manual_record.manual_id,
+        )
+        service.call
       rescue DocumentAssociationMarshaller::RemovedDocumentIdNotFoundError => e
         logger.error("Did not publish manual with id=#{manual_record.manual_id} slug=#{manual_record.slug}. It has at least one removed document which was not found: #{e.message}")
         next
