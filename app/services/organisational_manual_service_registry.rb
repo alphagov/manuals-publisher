1757b25ed5fd59175671eaa5a806f0ebe98c97f5
James Mead <james@floehopper.org>
2017-03-15 15:03:05 +0000

    Remove unused service registry classes

--- app/services/organisational_manual_service_registry.rb
+++
@@ -1,7 +0,0 @@
-class OrganisationalManualServiceRegistry < AbstractManualServiceRegistry
-  def initialize(organisation_slug:)
-    @organisation_slug = organisation_slug
-  end
-
-  attr_reader :organisation_slug
-end
