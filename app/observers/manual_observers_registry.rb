f4d5727bf9bc386bf4a30a892ddc85635967b794
Chris Lowis <chris.lowis@gmail.com>
2017-03-22 14:43:41 +0000

    Remove empty ManualObserversRegistry class
    The functionality of this class has now been removed, so it can be
    deleted.

--- app/observers/manual_observers_registry.rb
+++
@@ -1,9 +0,0 @@
-require 'publication_logger'
-require 'publishing_api_draft_manual_with_sections_exporter'
-require 'publishing_api_manual_with_sections_publisher'
-require 'rummager_manual_with_sections_exporter'
-require 'rummager_manual_with_sections_withdrawer'
-require 'publishing_api_manual_with_sections_withdrawer'
-
-class ManualObserversRegistry
-end
