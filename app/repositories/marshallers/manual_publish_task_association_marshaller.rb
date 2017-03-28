e1c33301d21a053933b2e334566e4c859171bd48
Chris Lowis <chris.lowis@gmail.com>
2017-03-28 15:45:58 +0100

    Refactor ManualPublishTaskAssociationMarshaller
    Remove the unnecessary use of an anonymous function for `decorator`
    and remove unused methods.

--- app/repositories/marshallers/manual_publish_task_association_marshaller.rb
+++ app/repositories/marshallers/manual_publish_task_association_marshaller.rb
@@ -1,25 +1,12 @@
 class ManualPublishTaskAssociationMarshaller
-  def initialize
-    @decorator = ->(manual, attrs) {
-      ManualWithPublishTasks.new(
-        manual,
-        attrs,
-      )
-    }
-  end
-
   def load(manual, _record)
     tasks = ManualPublishTask.for_manual(manual)
 
-    decorator.call(manual, publish_tasks: tasks)
+    ManualWithPublishTasks.new(manual, publish_tasks: tasks)
   end
 
   def dump(_manual, _record)
     # PublishTasks are read only
     nil
   end
-
-private
-
-  attr_reader :decorator
 end
