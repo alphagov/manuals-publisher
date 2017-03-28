7c49002649556b5864a368495ec3f1948e8bdd20
James Mead <james@floehopper.org>
2017-03-28 10:12:14 +0100

    Remove duplicate/unused feature helper methods
    These methods in `DocumentHelpers` are identical to methods in
    `ManualHelpers` and can be removed.

--- features/support/document_helpers.rb
+++ features/support/document_helpers.rb
@@ -7,16 +7,6 @@ module DocumentHelpers
     click_button("Preview")
   end
 
-  def check_content_preview_link(slug)
-    preview_url = "#{Plek.current.find('draft-origin')}/#{slug}"
-    expect(page).to have_link("Preview draft", href: preview_url)
-  end
-
-  def check_live_link(slug)
-    live_url = "#{Plek.current.website_root}/#{slug}"
-    expect(page).to have_link("View on website", href: live_url)
-  end
-
   def check_for_javascript_usage_error(field)
     expect(page).to have_content("#{field} cannot include invalid Govspeak, invalid HTML, any JavaScript or images hosted on sites except for")
   end
