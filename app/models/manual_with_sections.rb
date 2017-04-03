213ba6b1fe8a2081673d707ad4e74e5e8e7e0bdc
James Mead <james@floehopper.org>
2017-04-03 11:00:52 +0100

    Use array accessors throughout ManualWithSections
    Instead of using the instance variables. Again this should make it
    easier to move methods in `MamualWithSections` up into `Manual` one at
    a time.

--- app/models/manual_with_sections.rb
+++ app/models/manual_with_sections.rb
@@ -7,29 +7,29 @@ class ManualWithSections
   end
 
   def reorder_sections(section_order)
-    unless section_order.sort == @sections.map(&:id).sort
+    unless section_order.sort == sections.map(&:id).sort
       raise(
         ArgumentError,
         "section_order must contain each section_id exactly once",
       )
     end
 
-    @sections.sort_by! { |sec| section_order.index(sec.id) }
+    sections.sort_by! { |sec| section_order.index(sec.id) }
   end
 
   def remove_section(section_id)
-    found_section = @sections.find { |d| d.id == section_id }
+    found_section = sections.find { |d| d.id == section_id }
 
     return if found_section.nil?
 
-    removed = @sections.delete(found_section)
+    removed = sections.delete(found_section)
 
     return if removed.nil?
 
-    @removed_sections << removed
+    removed_sections << removed
   end
 
   def add_section(section)
-    @sections << section
+    sections << section
   end
 end
