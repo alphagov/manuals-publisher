26f55c19c60e99599caca976233dbc2ead09d488
Chris Lowis <chris.lowis@gmail.com>
2017-04-05 14:30:29 +0100

    Remove ScopedManualRepository
    ScopedManualRepository delegated all of its methods to
    ManualRepository and added no additional behaviour, so it can be
    removed.

--- app/repositories/scoped_manual_repository.rb
+++
@@ -1,9 +0,0 @@
-class ScopedManualRepository
-  extend Forwardable
-
-  def_delegators :@repository, :all, :store, :[], :fetch, :slug_unique?
-
-  def initialize(collection)
-    @repository = ManualRepository.new(collection)
-  end
-end
