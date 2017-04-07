45f1b9f637f67c4cfa53ae476fa89781e2db6223
James Mead <james@floehopper.org>
2017-04-07 13:12:25 +0100

    Simplify ManualRepository#fetch
    This method is only ever called from `Manual.find` and it's not called
    with a default value or a block.

--- app/repositories/manual_repository.rb
+++ app/repositories/manual_repository.rb
@@ -5,19 +5,10 @@ class ManualRepository
     @collection = collection
   end
 
-  NO_DEFAULT_GIVEN = Object.new
-
-  def fetch(key, not_found_value = NO_DEFAULT_GIVEN)
-    if not_found_value != NO_DEFAULT_GIVEN && block_given?
-      raise ArgumentError.new("Cannot provide both a default arg and block to #fetch")
-    end
-
+  def fetch(key)
     result = public_send(:[], key)
 
     if result.nil?
-      return yield(key) if block_given?
-      return not_found_value unless not_found_value == NO_DEFAULT_GIVEN
-
       raise KeyError.new("key not found #{key}")
     else
       result
