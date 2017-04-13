439cbb1a3eec8c6b48c9e4d41059b45a4b5728ab
Chris Roos <chris@seagul.co.uk>
2017-04-13 15:50:48 +0100

    Revert "Make cucumber features break to see whether they are running on CI"
    This reverts commit c99d415be902def824c471f81eb23f156684ed8f.

--- features/step_definitions/access_control_steps.rb
+++ features/step_definitions/access_control_steps.rb
@@ -1,7 +1,6 @@
 Given(/^I am logged in as a GDS editor$/) do
   login_as(:gds_editor)
   stub_organisation_details(GDS::SSO.test_user.organisation_slug)
-  raise "broken"
 end
 
 Given(/^I am logged in as an editor$/) do
