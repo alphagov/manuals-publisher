8aa4f7daa1d85fe1fcc02abc493c680c0ef568ee
James Mead <james@floehopper.org>
2017-04-24 10:47:56 +0100

    Rename doc -> section in Section spec
    This was missed when `SpecialistDocument` was renamed to `Section`.

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Section do
-  subject(:doc) {
+  subject(:section) {
     Section.new(slug_generator, section_id, editions, edition_factory)
   }
 
@@ -108,13 +108,13 @@ describe Section do
     let(:editions) { [draft_edition_v1] }
 
     it "is considered the same as another section instance if they have the same id" do
-      expect(doc).to eql(doc)
-      expect(doc).to eql(Section.new(slug_generator, doc.id, [draft_edition_v1]))
-      expect(doc).not_to eql(Section.new(slug_generator, doc.id.reverse, [draft_edition_v1]))
+      expect(section).to eql(section)
+      expect(section).to eql(Section.new(slug_generator, section.id, [draft_edition_v1]))
+      expect(section).not_to eql(Section.new(slug_generator, section.id.reverse, [draft_edition_v1]))
     end
 
     it "is considered the same as another section instance with the same id even if they have different version numbers" do
-      expect(doc).to eql(Section.new(slug_generator, doc.id, [draft_edition_v2]))
+      expect(section).to eql(Section.new(slug_generator, section.id, [draft_edition_v2]))
     end
   end
 
@@ -122,15 +122,15 @@ describe Section do
     let(:editions) { [draft_edition_v1] }
 
     it "is in draft" do
-      expect(doc).to be_draft
+      expect(section).to be_draft
     end
 
     it "is not published" do
-      expect(doc).not_to be_published
+      expect(section).not_to be_published
     end
 
     it "has never been published" do
-      expect(doc).not_to have_ever_been_published
+      expect(section).not_to have_ever_been_published
     end
   end
 
@@ -138,18 +138,18 @@ describe Section do
     let(:editions) { [published_edition_v1] }
 
     it "is published" do
-      expect(doc).to be_published
+      expect(section).to be_published
     end
 
     it "is not in draft" do
-      expect(doc).not_to be_draft
+      expect(section).not_to be_draft
     end
 
     context "that has been exported" do
       before { allow(published_edition_v1).to receive(:exported_at).and_return(4.days.ago) }
 
       it "has ever been published" do
-        expect(doc).to have_ever_been_published
+        expect(section).to have_ever_been_published
       end
     end
 
@@ -157,7 +157,7 @@ describe Section do
       before { allow(published_edition_v1).to receive(:exported_at).and_return(nil) }
 
       it "has never been published" do
-        expect(doc).not_to have_ever_been_published
+        expect(section).not_to have_ever_been_published
       end
     end
   end
@@ -166,12 +166,12 @@ describe Section do
     let(:editions) { [published_edition_v1, draft_edition_v2] }
 
     it "is published and in draft" do
-      expect(doc).to be_draft
-      expect(doc).to be_published
+      expect(section).to be_draft
+      expect(section).to be_published
     end
 
     it "has ever been published" do
-      expect(doc).to have_ever_been_published
+      expect(section).to have_ever_been_published
     end
   end
 
@@ -179,15 +179,15 @@ describe Section do
     let(:editions) { [draft_edition_v1, draft_edition_v2] }
 
     it "is in draft" do
-      expect(doc).to be_draft
+      expect(section).to be_draft
     end
 
     it "is not published" do
-      expect(doc).not_to be_published
+      expect(section).not_to be_published
     end
 
     it "has never been published" do
-      expect(doc).not_to have_ever_been_published
+      expect(section).not_to have_ever_been_published
     end
   end
 
@@ -195,15 +195,15 @@ describe Section do
     let(:editions) { [draft_edition_v1, withdrawn_edition_v2] }
 
     it "is not in draft" do
-      expect(doc).not_to be_draft
+      expect(section).not_to be_draft
     end
 
     it "is not published" do
-      expect(doc).not_to be_published
+      expect(section).not_to be_published
     end
 
     it "has never been published" do
-      expect(doc).not_to have_ever_been_published
+      expect(section).not_to have_ever_been_published
     end
   end
 
@@ -217,7 +217,7 @@ describe Section do
       }
 
       it "symbolizes the keys" do
-        doc.update(string_keyed_attrs)
+        section.update(string_keyed_attrs)
 
         expect(draft_edition_v1).to have_received(:assign_attributes).with(
           hash_including(body: "o hai")
@@ -234,7 +234,7 @@ describe Section do
       }
 
       it "cleans the hash" do
-        doc.update(bad_attrs)
+        section.update(bad_attrs)
 
         expect(draft_edition_v1).to have_received(:assign_attributes).with({})
       end
@@ -245,7 +245,7 @@ describe Section do
       let(:attrs)    { { title: "Test title" } }
 
       it "creates the first edition" do
-        doc.update(attrs)
+        section.update(attrs)
 
         expect(edition_factory).to have_received(:call).with(
           version_number: 1,
@@ -264,13 +264,13 @@ describe Section do
           let(:slug)      { double(:slug) }
 
           it "generates a slug" do
-            doc.update(title: new_title)
+            section.update(title: new_title)
 
             expect(slug_generator).to have_received(:call).with(new_title)
           end
 
           it "assigns the title and slug to the draft edition" do
-            doc.update(title: new_title)
+            section.update(title: new_title)
 
             expect(draft_edition_v1).to have_received(:assign_attributes)
               .with(
@@ -304,20 +304,20 @@ describe Section do
       end
 
       it "builds a new edition with the new params" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call).with(hash_including(params))
       end
 
       it "builds the new edition with attributes carried over from the previous edition" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call)
           .with(hash_including(body: edition_body))
       end
 
       it "filters the previous edition's attributes" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).not_to have_received(:call)
           .with(
@@ -337,32 +337,32 @@ describe Section do
       end
 
       it "builds a new edition with an incremented version number" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call).with(hash_including(version_number: 2))
       end
 
       it "builds a new edition in the 'draft' state" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call).with(hash_including(state: "draft"))
       end
 
       it "builds a new edition copying over the previous edition's attachments" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call)
           .with(hash_including(attachments: attachments))
       end
 
       it "presents the new edition" do
-        doc.update(params)
+        section.update(params)
 
-        expect(doc.version_number).to eq(new_edition.version_number)
+        expect(section.version_number).to eq(new_edition.version_number)
       end
 
       it "returns nil" do
-        expect(doc.update(params)).to eq(nil)
+        expect(section.update(params)).to eq(nil)
       end
 
       context "when providing a title" do
@@ -370,7 +370,7 @@ describe Section do
         let(:slug)      { double(:slug) }
 
         it "does not update the slug" do
-          doc.update(title: new_title)
+          section.update(title: new_title)
 
           expect(edition_factory).to have_received(:call).with(
             hash_including(
@@ -387,38 +387,38 @@ describe Section do
       let(:params) { { title: "It is a new title" } }
 
       it "builds a new edition with the new params" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call).with(hash_including(params))
       end
 
       it "builds a new edition with an incremented version number" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call).with(hash_including(version_number: 3))
       end
 
       it "builds a new edition in the 'draft' state" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call).with(hash_including(state: "draft"))
       end
 
       it "builds a new edition copying over the previous edition's attachments" do
-        doc.update(params)
+        section.update(params)
 
         expect(edition_factory).to have_received(:call)
           .with(hash_including(attachments: attachments))
       end
 
       it "presents the new edition" do
-        doc.update(params)
+        section.update(params)
 
-        expect(doc.version_number).to eq(new_edition.version_number)
+        expect(section.version_number).to eq(new_edition.version_number)
       end
 
       it "returns nil" do
-        expect(doc.update(params)).to eq(nil)
+        expect(section.update(params)).to eq(nil)
       end
 
       context "when providing a title" do
@@ -426,7 +426,7 @@ describe Section do
         let(:slug)      { double(:slug) }
 
         it "does not update the slug" do
-          doc.update(title: new_title)
+          section.update(title: new_title)
 
           expect(edition_factory).to have_received(:call).with(
             hash_including(
@@ -443,7 +443,7 @@ describe Section do
       let(:editions) { [draft_edition_v1] }
 
       it "should set its state to published" do
-        doc.publish!
+        section.publish!
         expect(draft_edition_v1).to have_received(:publish)
       end
     end
@@ -452,12 +452,12 @@ describe Section do
       let(:editions) { [published_edition_v1, draft_edition_v2] }
 
       it "should set the draft edition's state to published" do
-        doc.publish!
+        section.publish!
         expect(draft_edition_v2).to have_received(:publish)
       end
 
       it "archives the previous edition" do
-        doc.publish!
+        section.publish!
 
         expect(published_edition_v1).to have_received(:archive)
       end
@@ -467,7 +467,7 @@ describe Section do
       let(:editions) { [published_edition_v1] }
 
       it "do nothing" do
-        doc.publish!
+        section.publish!
         expect(published_edition_v1).not_to have_received(:publish)
       end
     end
@@ -478,7 +478,7 @@ describe Section do
     let(:params) { double(:params) }
 
     it "tells the latest edition to create an attachment using the supplied parameters" do
-      doc.add_attachment(params)
+      section.add_attachment(params)
 
       expect(draft_edition_v2).to have_received(:build_attachment).with(params)
     end
@@ -488,13 +488,13 @@ describe Section do
     let(:editions) { [published_edition_v1, draft_edition_v2] }
 
     it "delegates to the latest edition" do
-      doc.attachments
+      section.attachments
 
       expect(draft_edition_v2).to have_received(:attachments)
     end
 
     it "returns the attachments from the latest edition" do
-      expect(doc.attachments).to eq(attachments)
+      expect(section.attachments).to eq(attachments)
     end
   end
 
@@ -520,23 +520,23 @@ describe Section do
     let(:editions) { [published_edition_v1, edition] }
 
     it "symbolizes the keys" do
-      expect(key_classes_for(doc.attributes)).to eq([Symbol])
+      expect(key_classes_for(section.attributes)).to eq([Symbol])
     end
 
     it "returns attributes with junk removed" do
-      expect(doc.attributes).not_to include(
+      expect(section.attributes).not_to include(
         undesirable_edtion_attrs.symbolize_keys
       )
     end
 
     it "returns the latest edition's attributes" do
-      expect(doc.attributes).to include(
+      expect(section.attributes).to include(
         relevant_section_attrs.symbolize_keys
       )
     end
 
     it "returns a has including the section's id" do
-      expect(doc.attributes).to include(
+      expect(section.attributes).to include(
         id: section_id,
       )
     end
@@ -562,13 +562,13 @@ describe Section do
 
     it "returns the attachment with the corresponding id" do
       expect(
-        doc.find_attachment_by_id("one")
+        section.find_attachment_by_id("one")
       ).to eq(attachment_one)
     end
 
     it "returns nil if the attachment does not exist" do
       expect(
-        doc.find_attachment_by_id("does-not-exist")
+        section.find_attachment_by_id("does-not-exist")
       ).to be_nil
     end
   end
@@ -578,7 +578,7 @@ describe Section do
       let(:editions) { [draft_edition_v1] }
 
       it "returns 'draft'" do
-        expect(doc.publication_state).to eq("draft")
+        expect(section.publication_state).to eq("draft")
       end
     end
 
@@ -586,7 +586,7 @@ describe Section do
       let(:editions) { [published_edition_v1] }
 
       it "returns 'published'" do
-        expect(doc.publication_state).to eq("published")
+        expect(section.publication_state).to eq("published")
       end
     end
 
@@ -594,7 +594,7 @@ describe Section do
       let(:editions) { [published_edition_v1, draft_edition_v2] }
 
       it "returns 'published'" do
-        expect(doc.publication_state).to eq("published")
+        expect(section.publication_state).to eq("published")
       end
     end
 
@@ -602,7 +602,7 @@ describe Section do
       let(:editions) { [published_edition_v1, withdrawn_edition_v2] }
 
       it "returns 'withdrawn'" do
-        expect(doc.publication_state).to eq("withdrawn")
+        expect(section.publication_state).to eq("withdrawn")
       end
     end
   end
@@ -612,7 +612,7 @@ describe Section do
       let(:editions) { [draft_edition_v1] }
 
       it "returns false" do
-        expect(doc).not_to be_withdrawn
+        expect(section).not_to be_withdrawn
       end
     end
 
@@ -620,7 +620,7 @@ describe Section do
       let(:editions) { [published_edition_v1] }
 
       it "returns false" do
-        expect(doc).not_to be_withdrawn
+        expect(section).not_to be_withdrawn
       end
     end
 
@@ -628,7 +628,7 @@ describe Section do
       let(:editions) { [published_edition_v1, withdrawn_edition_v2] }
 
       it "returns true" do
-        expect(doc).to be_withdrawn
+        expect(section).to be_withdrawn
       end
     end
 
@@ -636,7 +636,7 @@ describe Section do
       let(:editions) { [published_edition_v1, withdrawn_edition_v2, draft_edition_v3] }
 
       it "returns false" do
-        expect(doc).not_to be_withdrawn
+        expect(section).not_to be_withdrawn
       end
     end
   end
@@ -646,7 +646,7 @@ describe Section do
       let(:editions) { [draft_edition_v1] }
 
       it "archives the draft" do
-        doc.withdraw_and_mark_as_exported!
+        section.withdraw_and_mark_as_exported!
 
         expect(draft_edition_v1).to have_received(:archive)
       end
@@ -654,7 +654,7 @@ describe Section do
       it "sets the exported_at date on the draft" do
         time = Time.zone.now
         Timecop.freeze(time) do
-          doc.withdraw_and_mark_as_exported!
+          section.withdraw_and_mark_as_exported!
           expect(draft_edition_v1).to have_received(:exported_at=).with(time)
         end
       end
@@ -664,7 +664,7 @@ describe Section do
       let(:editions) { [published_edition_v1, withdrawn_edition_v2] }
 
       it "does nothing to the states of the editions" do
-        doc.withdraw_and_mark_as_exported!
+        section.withdraw_and_mark_as_exported!
 
         expect(published_edition_v1).not_to have_received(:archive)
         expect(withdrawn_edition_v2).not_to have_received(:archive)
@@ -673,7 +673,7 @@ describe Section do
       it "only sets the exported_at date on the withdrawn edition" do
         time = Time.zone.now
         Timecop.freeze(time) do
-          doc.withdraw_and_mark_as_exported!
+          section.withdraw_and_mark_as_exported!
           expect(withdrawn_edition_v2).to have_received(:exported_at=).with(time)
 
           expect(published_edition_v1).not_to have_received(:exported_at=)
@@ -685,7 +685,7 @@ describe Section do
       let(:editions) { [published_edition_v1, draft_edition_v2] }
 
       it "sets the draft edition's state to withdrawn" do
-        doc.withdraw_and_mark_as_exported!
+        section.withdraw_and_mark_as_exported!
 
         expect(draft_edition_v2).to have_received(:archive)
       end
@@ -693,7 +693,7 @@ describe Section do
       it "only sets the exported_at date on the draft edition" do
         time = Time.zone.now
         Timecop.freeze(time) do
-          doc.withdraw_and_mark_as_exported!
+          section.withdraw_and_mark_as_exported!
           expect(draft_edition_v2).to have_received(:exported_at=).with(time)
 
           expect(published_edition_v1).not_to have_received(:exported_at=)
@@ -705,7 +705,7 @@ describe Section do
       let(:editions) { [published_edition_v1] }
 
       it "sets the published edition's state to withdrawn" do
-        doc.withdraw_and_mark_as_exported!
+        section.withdraw_and_mark_as_exported!
 
         expect(published_edition_v1).to have_received(:archive)
       end
@@ -713,7 +713,7 @@ describe Section do
       it "sets the exported_at date on the published edition" do
         time = Time.zone.now
         Timecop.freeze(time) do
-          doc.withdraw_and_mark_as_exported!
+          section.withdraw_and_mark_as_exported!
           expect(published_edition_v1).to have_received(:exported_at=).with(time)
         end
       end
@@ -726,7 +726,7 @@ describe Section do
     it "sets the exported_at date on the latest edition" do
       time = Time.zone.now
       Timecop.freeze(time) do
-        doc.mark_as_exported!
+        section.mark_as_exported!
         expect(draft_edition_v2).to have_received(:exported_at=).with(time).ordered
         expect(draft_edition_v2).to have_received(:save).ordered
 
@@ -734,5 +734,15 @@ describe Section do
         expect(published_edition_v1).not_to have_received(:save)
       end
     end
+  end
+
+  describe "#change_note_not_required?" do
+    it 'foo' do
+
+    end
+  end
+
+  describe "#change_note_provided?" do
+
   end
 end
