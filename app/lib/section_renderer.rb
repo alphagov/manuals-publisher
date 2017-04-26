3afaad2a2268c98a449fdd56d08b7d6ec69d8806
Chris Lowis <chris.lowis@gmail.com>
2017-04-26 11:46:02 +0100

    Remove SectionRenderer
    This class is no longer used and can be removed.

--- app/lib/section_renderer.rb
+++
@@ -1,9 +0,0 @@
-require "markdown_attachment_processor"
-require "section_header_extractor"
-require "govspeak_to_html_renderer"
-require "footnotes_section_heading_renderer"
-
-class SectionRenderer
-  def call(doc)
-  end
-end
