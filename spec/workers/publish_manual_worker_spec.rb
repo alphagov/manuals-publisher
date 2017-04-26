020f4520b51c7b636e4669bdd9c2d1637c57856d
Chris Roos <chris@seagul.co.uk>
2017-04-26 12:49:03 +0100

    Add test for recoverable errors in PublishManualWorker
    I want to log these errors to the Rails log as well as Airbrake. I'm
    adding these unit tests first to make it easier to test my new behaviour
    in a subsequent commit.
    
    The raising of a `FailedToPublishError` is also being tested in the
    "Manual publication retries after recoverable error" scenario of the
    publishing-a-manual Cucumber feature but that doesn't feel like the
    right place for me to test the logging I want to add.

--- spec/workers/publish_manual_worker_spec.rb
+++ spec/workers/publish_manual_worker_spec.rb
@@ -25,4 +25,27 @@ RSpec.describe PublishManualWorker do
       expect(GdsApi::GovukHeaders.headers[:x_govuk_authenticated_user]).to eq("abc123")
     end
   end
+
+  context 'when encountering an HTTP server error connecting to the GDS API' do
+    let(:publish_service) { double(:publish_service) }
+    let(:task) { ManualPublishTask.create! }
+    let(:worker) { PublishManualWorker.new }
+    let(:http_error) { GdsApi::HTTPServerError.new(500) }
+
+    before do
+      allow(Manual::PublishService).to receive(:new).and_return(publish_service)
+      allow(publish_service).to receive(:call).and_raise(http_error)
+    end
+
+    it 'raises a failed to publish error so that Sidekiq can retry the job' do
+      expect { worker.perform(task.id) }
+        .to raise_error(PublishManualWorker::FailedToPublishError)
+    end
+
+    it 'notifies Airbrake of the error' do
+      expect(Airbrake).to receive(:notify).with(http_error)
+
+      worker.perform(task.id) rescue PublishManualWorker::FailedToPublishError
+    end
+  end
 end
