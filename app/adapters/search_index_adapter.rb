0a9460d229b54e6c3096dcb57b9532b61180df73
James Mead <james@floehopper.org>
2017-04-27 12:09:13 +0100

    Inline RummagerManualWithSectionsExporter#call

--- app/adapters/search_index_adapter.rb
+++ app/adapters/search_index_adapter.rb
@@ -1,11 +1,31 @@
+require "rummager_indexer"
+
 class SearchIndexAdapter
   def initialize
-    @exporter = RummagerManualWithSectionsExporter.new
     @withdrawer = RummagerManualWithSectionsWithdrawer.new
   end
 
   def add(manual)
-    @exporter.call(manual)
+    indexer = RummagerIndexer.new
+
+    indexer.add(
+      ManualIndexableFormatter.new(manual)
+    )
+
+    manual.sections.each do |section|
+      indexer.add(
+        SectionIndexableFormatter.new(
+          MarkdownAttachmentProcessor.new(section),
+          manual,
+        )
+      )
+    end
+
+    manual.removed_sections.each do |section|
+      indexer.delete(
+        SectionIndexableFormatter.new(section, manual),
+      )
+    end
   end
 
   def remove(manual)
