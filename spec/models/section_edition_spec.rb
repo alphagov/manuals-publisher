ff509299ef5b8b367cd55457f195a6cc5d56f1f1
Chris Roos <chris@seagul.co.uk>
2017-04-27 15:23:03 +0100

    Add tests for SectionEdition validation
    To give me some additional confidence when I come to rename
    `SectionEdition#section_id`.

--- spec/models/section_edition_spec.rb
+++ spec/models/section_edition_spec.rb
@@ -6,4 +6,24 @@ describe SectionEdition do
   it 'stores data in the manual_section_editions collection' do
     expect(subject.collection.name).to eq('manual_section_editions')
   end
+
+  describe 'validation' do
+    it 'is valid if section_id and slug are present' do
+      subject.section_id = 'section-id'
+      subject.slug = 'section-slug'
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid if section_id is missing' do
+      subject.section_id = nil
+      expect(subject).not_to be_valid
+      expect(subject.errors[:section_id]).to include("can't be blank")
+    end
+
+    it 'is invalid if slug is missing' do
+      subject.slug = nil
+      expect(subject).not_to be_valid
+      expect(subject.errors[:slug]).to include("can't be blank")
+    end
+  end
 end
