8117e7e77440fd246a672458ca7eaaf644e60cbf
James Mead <james@floehopper.org>
2017-05-02 17:03:52 +0100

    Add unit spec for SearchIndexAdapter
    As requested by @chrisroos.
    
    I've chosen to only stub at the level of the Rummager API itself. Thus
    this is actually more like an integration test at the moment, because
    it's testing the behaviour of the combination of the following:
    
    * `SearchIndexAdapter`
    * `RummagerIndexer`
    * `AbstractIndexableFormatter`
    * `ManualIndexableFormatter`
    * `SectionIndexableFormatter`
    
    However, I'm hoping we can gradually collapse the last four classes into
    the `SearchIndexAdapter`. So writing the tests like this is a bit of an
    investment in making it easier to refactor in the future.
    
    I've intentionally *not* stubbed the Rummager API using the
    WebMock-based `GdsApi::TestHelpers::Rummager` helper methods, because
    I think the latter is too low level and would unnecessarily duplicate
    tests which should live in the `gds-api-adapters` gem.

---
+++ spec/adapters/search_index_adapter_spec.rb
@@ -0,0 +1,141 @@
+require "spec_helper"
+
+describe SearchIndexAdapter do
+  let(:rummager) { double(:rummager) }
+
+  let(:rummager_document_type_for_manual) { ManualIndexableFormatter::RUMMAGER_DOCUMENT_TYPE }
+  let(:rummager_document_type_for_section) { SectionIndexableFormatter::RUMMAGER_DOCUMENT_TYPE }
+
+  let(:manual) {
+    Manual.build(
+      id: "manual-id",
+      slug: "manual-slug",
+      title: "manual-title",
+      summary: "manual-summary",
+      updated_at: Time.zone.parse("2017-01-01 00:00:00")
+    )
+  }
+
+  let(:section) {
+    Section.build(
+      manual: manual,
+      id: "section-id",
+      editions: [section_edition],
+    )
+  }
+
+  let(:section_edition) {
+    SectionEdition.new(
+      slug: "manual-slug/section-slug",
+      title: "section-title",
+      summary: "section-summary",
+      body: "section-body"
+    )
+  }
+
+  let(:removed_section) {
+    Section.build(
+      manual: manual,
+      id: "removed-section-id",
+      editions: [removed_section_edition],
+    )
+  }
+
+  let(:removed_section_edition) {
+    SectionEdition.new(
+      slug: "manual-slug/removed-section-slug"
+    )
+  }
+
+  before do
+    allow(Services).to receive(:rummager).and_return(rummager)
+  end
+
+  describe "#add" do
+    before do
+      manual.sections = [section]
+      manual.removed_sections = [removed_section]
+
+      allow(rummager).to receive(:add_document).with(
+        rummager_document_type_for_manual, anything, anything
+      )
+      allow(rummager).to receive(:add_document).with(
+        rummager_document_type_for_section, anything, anything
+      )
+      allow(rummager).to receive(:delete_document).with(
+        rummager_document_type_for_section, anything
+      )
+    end
+
+    it "adds manual to Rummager search index" do
+      expect(rummager).to receive(:add_document).with(
+        rummager_document_type_for_manual,
+        "/manual-slug",
+        title: "manual-title",
+        description: "manual-summary",
+        link: "/manual-slug",
+        indexable_content: "manual-summary",
+        public_timestamp: manual.updated_at,
+        content_store_document_type: rummager_document_type_for_manual
+      )
+
+      subject.add(manual)
+    end
+
+    it "adds all manual's sections to Rummager search index" do
+      expect(rummager).to receive(:add_document).with(
+        rummager_document_type_for_section,
+        "/manual-slug/section-slug",
+        title: "manual-title: section-title",
+        description: "section-summary",
+        link: "/manual-slug/section-slug",
+        indexable_content: "section-body",
+        public_timestamp: nil,
+        content_store_document_type: rummager_document_type_for_section,
+        manual: "/manual-slug"
+      )
+
+      subject.add(manual)
+    end
+
+    it "removes manual's removed_sections from Rummager search index" do
+      expect(rummager).to receive(:delete_document).with(
+        rummager_document_type_for_section,
+        "/manual-slug/removed-section-slug"
+      )
+
+      subject.add(manual)
+    end
+  end
+
+  describe "#remove" do
+    before do
+      manual.sections = [section]
+
+      allow(rummager).to receive(:delete_document).with(
+        rummager_document_type_for_manual, anything
+      )
+      allow(rummager).to receive(:delete_document).with(
+        rummager_document_type_for_section, anything
+      )
+    end
+
+    it "removes manual from Rummager search index" do
+      expect(rummager).to receive(:delete_document).with(
+        rummager_document_type_for_manual,
+        "/manual-slug",
+      )
+
+      subject.remove(manual)
+    end
+
+    it "removes all manual's sections from Rummager search index" do
+      expect(rummager).to receive(:delete_document).with(
+        rummager_document_type_for_section,
+        "/manual-slug/section-slug",
+      )
+
+      subject.remove(manual)
+    end
+  end
+end
