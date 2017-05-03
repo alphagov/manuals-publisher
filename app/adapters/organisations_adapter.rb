31dc423dfaf4b8ef8d9eb87e6e1e77a00d4a5501
James Mead <james@floehopper.org>
2017-05-03 17:56:10 +0100

    Inline OrganisationFetcher into OrganisationsAdapter
    I think it's slightly clearer to instantiate the Organisations API in
    the constructor.
    
    I think it's also clearer to instance the cache of organisations in the
    constructor and to name it more expressively, `@cache`.
    
    Note that the `OrganisationsAdapter` spec already includes coverage for
    the caching behaviour.

--- app/adapters/organisations_adapter.rb
+++ app/adapters/organisations_adapter.rb
@@ -1,11 +1,19 @@
 class OrganisationsAdapter
+  def initialize
+    @api = Services.organisations
+    @cache = {}
+  end
+
   def find(slug)
-    response = OrganisationFetcher.fetch(slug)
-    Organisation.new(
-      title: response["title"],
-      web_url: response["web_url"],
-      abbreviation: response["details"]["abbreviation"],
-      content_id: response["details"]["content_id"]
-    )
+    @cache.fetch(slug) do
+      response = @api.organisation(slug)
+      organisation = Organisation.new(
+        title: response["title"],
+        web_url: response["web_url"],
+        abbreviation: response["details"]["abbreviation"],
+        content_id: response["details"]["content_id"]
+      )
+      @cache[slug] = organisation
+    end
   end
 end
