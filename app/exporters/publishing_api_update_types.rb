9bae47726e41783e636e11ac9a9161cf7d4ade21
James Mead <james@floehopper.org>
2017-05-04 17:03:29 +0100

    Simplify logic in PublishingAPIUpdateTypes#check_update_type!
    I think this is easier to understand.

--- app/exporters/publishing_api_update_types.rb
+++ app/exporters/publishing_api_update_types.rb
@@ -2,7 +2,8 @@ module PublishingAPIUpdateTypes
   UPDATE_TYPES = %w(minor major republish).freeze
 
   def check_update_type!(update_type)
-    return if update_type.nil?
-    raise ArgumentError, "update_type '#{update_type}' not recognised" unless UPDATE_TYPES.include?(update_type)
+    if update_type.present? && !UPDATE_TYPES.include?(update_type)
+      raise ArgumentError, "update_type '#{update_type}' not recognised"
+    end
   end
 end
