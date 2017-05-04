e2d7224c4fdfb500a2d4f5a1fb3538fd93df7896
James Mead <james@floehopper.org>
2017-05-04 17:01:58 +0100

    Add blank line in PublishingAPIUpdateTypes
    To improve readability.

--- app/exporters/publishing_api_update_types.rb
+++ app/exporters/publishing_api_update_types.rb
@@ -1,5 +1,6 @@
 module PublishingAPIUpdateTypes
   UPDATE_TYPES = %w(minor major republish).freeze
+
   def check_update_type!(update_type)
     return if update_type.nil?
     raise ArgumentError, "update_type '#{update_type}' not recognised" unless UPDATE_TYPES.include?(update_type)
