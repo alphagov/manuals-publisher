878e96786b4a18e1f7593fd12311a6c3f7632cd1
Chris Lowis <chris.lowis@gmail.com>
2017-05-05 12:13:04 +0100

    Introduce specs for SectionPresenter
    These specs tests the main functionality of SectionPresenter. I've
    used an "integration" style of test to assert whether the rendered
    HTML in the body has been processed in the expected way rather than
    mocking the collaborators and asserting that they have been called.
    
    The intention of introducing these specs at this stage is to make it
    easier to refactor the SectionPresenter and inline some of the
    functionality from its collaborators.

---
+++ spec/presenters/section_presenter_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe SectionPresenter do
+  let(:section) { double(:section, body: '', attributes: {}, attachments: []) }
+  subject { described_class.new(section) }
+
+  describe '#body' do
+    context 'a section with a footnotes div' do
+      before do
+        allow(section).to receive(:body).and_return('<div class="footnotes"></div>')
+      end
+
+      it 'adds a title before the existing footnotes' do
+        expect(subject.body).to eq(%{<h2 id="footnotes">Footnotes</h2><div class="footnotes"></div>\n})
+      end
+    end
+
+    context 'a section with Govspeak in the body' do
+      before do
+        govspeak = <<-GOVSPEAK.strip_heredoc
+          $I
+            This is information
+          $I
+        GOVSPEAK
+
+        allow(section).to receive(:body).and_return(govspeak)
+      end
+
+      it 'converts the govspeak to HTML' do
+        expected_html = <<-HTML.strip_heredoc
+
+          <div class="information">
+          <p>This is information</p>
+          </div>
+        HTML
+
+        expect(subject.body).to eq(expected_html)
+      end
+    end
+
+    context 'a section with an attachment referenced by a snippet in the body' do
+      before do
+        attachment = double(:attachment, title: 'title', file_url: 'file-url', snippet: '[snippet]')
+
+        allow(section).to receive(:attachments).and_return([attachment])
+        allow(section).to receive(:body).and_return('[snippet]')
+      end
+
+      it 'replaces snippet with a link' do
+        expect(subject.body).to eq("<p><a href=\"file-url\">title</a></p>\n")
+      end
+    end
+  end
+end
