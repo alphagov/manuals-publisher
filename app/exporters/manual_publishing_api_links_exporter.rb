a1e9527e785749a141b5992129d932f35d2d7b6e
James Mead <james@floehopper.org>
2017-05-09 11:55:42 +0100

    Inline ManualPublishingAPILinksExporter#content_id

--- app/exporters/manual_publishing_api_links_exporter.rb
+++ app/exporters/manual_publishing_api_links_exporter.rb
@@ -5,16 +5,12 @@ class ManualPublishingAPILinksExporter
   end
 
   def call
-    Services.publishing_api.patch_links(content_id, exportable_attributes)
+    Services.publishing_api.patch_links(manual.id, exportable_attributes)
   end
 
 private
 
   attr_reader :organisation, :manual
-
-  def content_id
-    manual.id
-  end
 
   def exportable_attributes
     {
