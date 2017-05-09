7df5400fe2503b6eb9fc9bdbdc883a2c77692b1f
James Mead <james@floehopper.org>
2017-05-09 11:50:34 +0100

    Inline PublishingApiDraftManualWithSectionsExporter

--- app/adapters/publishing_adapter.rb
+++ app/adapters/publishing_adapter.rb
@@ -1,5 +1,29 @@
+require "adapters"
+
 class PublishingAdapter
   def save(manual, action = nil)
-    PublishingApiDraftManualWithSectionsExporter.new.call(manual, action)
+    update_type = (action == :republish ? "republish" : nil)
+
+    organisation = Adapters.organisations.find(manual.organisation_slug)
+
+    ManualPublishingAPILinksExporter.new(
+      organisation, manual
+    ).call
+
+    ManualPublishingAPIExporter.new(
+      organisation, manual, update_type: update_type
+    ).call
+
+    manual.sections.each do |section|
+      next if !section.needs_exporting? && action != :republish
+
+      SectionPublishingAPILinksExporter.new(
+        organisation, manual, section
+      ).call
+
+      SectionPublishingAPIExporter.new(
+        organisation, manual, section, update_type: update_type
+      ).call
+    end
   end
 end
