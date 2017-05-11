8729b04427ba346e72291ab205c5c7bb1a93ddce
James Mead <james@floehopper.org>
2017-05-11 15:37:54 +0100

    Inline PublishingAPIWithdrawer#exportable_attributes

--- app/exporters/publishing_api_withdrawer.rb
+++ app/exporters/publishing_api_withdrawer.rb
@@ -7,7 +7,7 @@ class PublishingAPIWithdrawer
 
   def call
     content_id = entity.is_a?(Section) ? entity.uuid : entity.id
-    Services.publishing_api.unpublish(content_id, exportable_attributes)
+    Services.publishing_api.unpublish(content_id, type: "gone")
   end
 
 private
@@ -15,10 +15,4 @@ private
   attr_reader(
     :entity,
   )
-
-  def exportable_attributes
-    {
-      type: "gone",
-    }
-  end
 end
