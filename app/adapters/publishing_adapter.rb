e8e80b1a5f126082ace14d73e3d297d879fc6599
James Mead <james@floehopper.org>
2017-05-11 15:13:11 +0100

    Don't swallow GdsApi exceptions in PublishingAdapter#discard_section
    As far as I can see this is method only ever called from
    `Section::RemoveService#discard_section_via_publishing_api`. This method
    is only called as part of the `SectionsController#destroy` action which
    is only accessible from the page rendered by the
    `SectionsController#withdraw` action. The latter action makes use of the
    `Section::ShowService` which will only work if a section has been
    created and consequently sent to the Publishing API. I think this means
    that we can expect that in normal operation these `GdsApi` exceptions
    will not be raised and if they are, we want to know about them. Thus I'm
    happy to remove this `rescue` block.

--- app/adapters/publishing_adapter.rb
+++ app/adapters/publishing_adapter.rb
@@ -43,7 +43,6 @@ class PublishingAdapter
 
   def discard_section(section)
     Services.publishing_api.discard_draft(section.uuid)
-  rescue GdsApi::HTTPNotFound, GdsApi::HTTPUnprocessableEntity # rubocop:disable Lint/HandleExceptions
   end
 
 private
