2c5537ca17acbc12e7bafc27d3754dd6b5c5313b
Chris Roos <chris@seagul.co.uk>
2017-05-23 15:51:23 +0100

    Inline Section::NewService#manual

--- app/services/section/new_service.rb
+++ app/services/section/new_service.rb
@@ -5,14 +5,11 @@ class Section::NewService
   end
 
   def call
+    manual = Manual.find(manual_id, user)
     [manual, manual.build_section({})]
   end
 
 private
 
   attr_reader :user, :manual_id
-
-  def manual
-    @manual ||= Manual.find(manual_id, user)
-  end
 end
