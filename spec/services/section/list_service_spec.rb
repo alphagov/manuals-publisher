7634846dfe8bf3c665356756a7527c2e08c8ca71
Chris Roos <chris@seagul.co.uk>
2017-05-23 16:35:48 +0100

    Add spec for Section::ListService
    This should make it easier to make changes to this service in future.

---
+++ spec/services/section/list_service_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+RSpec.describe Section::ListService do
+  it 'returns the manual and its sections' do
+    user = FactoryGirl.build(:user, organisation_slug: 'org-slug')
+    manual = FactoryGirl.build(:manual,
+      organisation_slug: user.organisation_slug)
+    section = manual.build_section(title: 'section-title')
+    manual.save(user)
+
+    service = Section::ListService.new(user: user, manual_id: manual.id)
+
+    returned_manual, returned_sections = service.call
+
+    expect(returned_manual).to eql(manual)
+    expect(returned_sections).to eql([section])
+  end
+end
