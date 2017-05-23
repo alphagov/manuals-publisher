b42f8af0a63dd3929383de7181b4b7e2225ba8db
Chris Roos <chris@seagul.co.uk>
2017-05-23 16:36:27 +0100

    Add spec for Section::NewService
    This should make it easier to make changes to this service in future.

---
+++ spec/services/section/new_service_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+RSpec.describe Section::NewService do
+  it 'returns the manual and a new section' do
+    user = FactoryGirl.build(:user, organisation_slug: 'org-slug')
+    manual = FactoryGirl.build(:manual, organisation_slug: user.organisation_slug)
+    manual.save(user)
+
+    service = Section::NewService.new(user: user, manual_id: manual.id)
+
+    returned_manual, returned_section = service.call
+
+    expect(returned_manual).to eql(manual)
+    expect(returned_manual.sections).to include(returned_section)
+  end
+end
