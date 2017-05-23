3bc4c666b64d82a5f13614afc4cd5844533c44b1
Chris Roos <chris@seagul.co.uk>
2017-05-23 15:40:22 +0100

    Inline Section::CreateService#export_draft_manual_to_publishing_api

--- app/services/section/create_service.rb
+++ app/services/section/create_service.rb
@@ -14,7 +14,7 @@ class Section::CreateService
     if new_section.valid?
       manual.draft
       manual.save(user)
-      export_draft_manual_to_publishing_api
+      Adapters.publishing.save(manual, include_sections: false)
       export_draft_section_to_publishing_api
     end
 
@@ -26,10 +26,6 @@ private
   attr_reader :user, :manual_id, :attributes
 
   attr_reader :new_section
-
-  def export_draft_manual_to_publishing_api
-    Adapters.publishing.save(manual, include_sections: false)
-  end
 
   def export_draft_section_to_publishing_api
     Adapters.publishing.save_section(new_section, manual)
