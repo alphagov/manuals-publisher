f9602be98e68a50fae873fbd42c135bb3246eedd
Chris Roos <chris@seagul.co.uk>
2017-05-29 09:17:03 +0200

    Inline Manual::PreviewService#manual
    Inlining these methods should make it easier to see the behaviour of the
    `#call` method.

--- app/services/manual/preview_service.rb
+++ app/services/manual/preview_service.rb
@@ -6,6 +6,11 @@ class Manual::PreviewService
   end
 
   def call
+    manual = if manual_id
+               Manual.find(manual_id, user)
+             else
+               Manual.new(attributes)
+             end
     manual.update(attributes)
 
     ManualPresenter.new(manual)
@@ -14,8 +19,4 @@ class Manual::PreviewService
 private
 
   attr_reader :user, :manual_id, :attributes
-
-  def manual
-    manual_id ? Manual.find(manual_id, user) : Manual.new(attributes)
-  end
 end
