22ebd922354a9940e2018e61bed11e31fcb6d77f
Seb Ashton <seb@sebashton.com>
2017-12-21 11:19:44 +0000

    Add LinkCheckReportsController#show

--- app/controllers/link_check_reports_controller.rb
+++ app/controllers/link_check_reports_controller.rb
@@ -11,9 +11,43 @@ class LinkCheckReportsController < ApplicationController
     head :created
   end
 
+  def show
+    @report = LinkCheckReport::ShowService.new(
+      id: link_reportable_show_params[:id]
+    ).call
+
+    @reportable = find_reportable(section_id: @report.section_id, manual_id: @report.manual_id)
+
+    respond_to do |format|
+      format.js { render 'admin/link_check_reports/show' }
+      format.html { redirect_to_reportable }
+    end
+  end
+
 private
 
   def link_reportable_params
     params.require(:link_reportable).permit(:manual_id, :section_id)
+  end
+
+  def link_reportable_show_params
+    params.permit(:id)
+  end
+
+  def find_reportable(reportable_params)
+    if reportable_params[:section_id] && reportable_params[:manual_id]
+      manual = Manual.find(reportable_params[:manual_id], current_user)
+      Section.find(manual, reportable_params[:manual_id])
+    elsif reportable_params[:manual_id]
+      Manual.find(reportable_params[:manual_id], current_user)
+    end
+  end
+
+  def redirect_to_reportable
+    if @reportable.is_a?(Section)
+      redirect_to manual_section_path(@reportable.manual.to_param, @reportable.to_param)
+    elsif @reportable.is_a?(Manual)
+      redirect_to manual_path(@reportable.to_param)
+    end
   end
 end
--- spec/controllers/link_check_reports_controller_spec.rb
+++ spec/controllers/link_check_reports_controller_spec.rb
@@ -2,11 +2,20 @@ require "spec_helper"
 require "services"
 
 describe LinkCheckReportsController, type: :controller do
-  describe "#create" do
-    let(:user) { create(:user) }
-    let(:manual) { double(:manual, id: 1, body: "[link](http://www.example.com)") }
-    let(:section) { FactoryGirl.create(:section_edition, id: 1) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:manual) { FactoryGirl.build(:manual, id: 538, body: "[link](http://[www.example.com)") }
+  let(:section) { FactoryGirl.create(:section_edition, id: 53880, body: "[link](http://[www.example.com/section)") }
 
+  before do
+    login_as_stub_user
+    allow(Manual).to receive(:find).and_return(manual)
+    allow(Section).to receive(:find).and_return(section)
+    allow(section).to receive(:manual).and_return(manual)
+    # This is needed as the real section will be_a Section
+    allow(section).to receive(:is_a?).and_return(Section)
+  end
+
+  describe "#create" do
     let(:link_checker_api_response) do
       {
         id: 1,
@@ -27,10 +36,7 @@ describe LinkCheckReportsController, type: :controller do
     end
 
     before do
-      login_as_stub_user
       allow(Services.link_checker_api).to receive(:create_batch).and_return(link_checker_api_response)
-      allow(Manual).to receive(:find).and_return(manual)
-      allow(Section).to receive(:find).and_return(section)
     end
 
     it "returns a created status" do
@@ -44,10 +50,53 @@ describe LinkCheckReportsController, type: :controller do
     end
   end
 
+
   describe "#show" do
-    it "shows the report" do
-      get :show, params: {} 
-      expect(response).to have_http_status(:success)
+    context "manual" do
+      let(:link_check_report) do
+        FactoryGirl.create(:link_check_report, :with_broken_links,
+                                                manual_id: manual.id,
+                                                batch_id: 1)
+      end
+
+      it "GET redirects back to the manual page" do
+        get :show, params: { id: link_check_report.id }
+
+        expected_path = manual_path(manual.to_param)
+        expect(response).to redirect_to(expected_path)
+      end
+
+      it "AJAX GET assigns the LinkCheckReport and renders the show template" do
+        get :show, xhr: true, params: { id: link_check_report.id }
+
+        expect(response).to render_template("admin/link_check_reports/show")
+        expect(assigns(:report)).to eq(link_check_report)
+        expect(assigns(:reportable)).to eq(manual)
+      end
+    end
+
+    context "section" do
+      let(:link_check_report) do
+        FactoryGirl.create(:link_check_report, :with_broken_links,
+                                                manual_id: manual.id,
+                                                section_id: section.id,
+                                                batch_id: 1)
+      end
+
+      it "GET redirects back to the section page" do
+        get :show, params: { id: link_check_report.id }
+
+        expected_path = manual_section_path(manual.to_param, section.to_param)
+        expect(response).to redirect_to(expected_path)
+      end
+
+      it "AJAX GET assigns the LinkCheckReport and renders the show template" do
+        get :show, xhr: true, params: { id: link_check_report.id }
+
+        expect(response).to render_template("admin/link_check_reports/show")
+        expect(assigns(:report)).to eq(link_check_report)
+        expect(assigns(:reportable)).to eq(section)
+      end
     end
   end
 end
