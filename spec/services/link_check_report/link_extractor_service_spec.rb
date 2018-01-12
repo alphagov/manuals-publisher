fe79d543923431731b9180318c87bed8fa7d4eae
Seb Ashton <seb@sebashton.com>
2018-01-12 11:50:54 +0000

    Create LinkExtractorService

---
+++ spec/services/link_check_report/link_extractor_service_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+RSpec.describe LinkCheckReport::LinkExtractorService do
+  let(:body) do
+    %{
+## Heading
+
+[link](http://www.example.com)
+
+[link_two](http://www.gov.com)
+
+[not_a_link](#somepage)
+
+[mailto:](mailto:someone@www.example.com)
+
+[absolute_path](/cais-trwydded-yrru-dros-dro)
+    }
+  end
+
+  let(:website_root) { Plek.new.website_root }
+
+  subject { described_class.new(body: body).call }
+
+  it "should contain three full urls" do
+    expected_result = %W{http://www.example.com http://www.gov.com #{website_root}/cais-trwydded-yrru-dros-dro}
+
+    expect(subject).to match(expected_result)
+  end
+
+  it "should not contain a mailto" do
+    expect(subject).not_to include("mailto:someone@www.example.com")
+  end
+
+  it "should not an anchor" do
+    expect(subject).not_to include("#somepage")
+  end
+end
