2bd8e5a3c43a85b3743a5641cc25cbbb8e4c70e0
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-02-09 11:06:02 +0000

    Explicitly use external URLs for Signon

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -2,5 +2,5 @@ GDS::SSO.config do |config|
   config.user_model   = "User"
   config.oauth_id     = ENV["OAUTH_ID"] || "not used"
   config.oauth_secret = ENV["OAUTH_SECRET"] || "not used"
-  config.oauth_root_url = Plek.current.find("signon")
+  config.oauth_root_url = Plek.new.external_url_for("signon")
 end
