78f9d4d81cad000fb2c29dc114a3004114c2b41a
Kevin Dew <kevindew@me.com>
2020-11-24 00:33:55 +0000

    Fix JS linting issues

--- app/assets/javascripts/ajax_setup.js
+++ app/assets/javascripts/ajax_setup.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $.ajaxSetup({
     headers: {
       'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
     }
-  });
-});
+  })
+})
