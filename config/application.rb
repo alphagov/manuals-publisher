0a02b72d8c730629d1e918a715d50a1a01b43872
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-09-28 17:15:05 +0100

    Fix CI
    Worked locally without this fix, but CI is complaining, so here
    we go.

--- config/application.rb
+++ config/application.rb
@@ -28,14 +28,16 @@ module ManualsPublisher
   end
 end
 
-Jasmine.configure do |config|
-  # existing config here
+if defined?(Jasmine)
+  Jasmine.configure do |config|
+    # existing config here
 
-  if ENV["SELENIUM_URL"]
-    # require "socket"
-    # ip = Socket.ip_address_list
-    #   .detect(&:ipv4_private?)
-    #   .ip_address
-    config.host = "http://manuals-publisher.dev.gov.uk/"
+    if ENV["SELENIUM_URL"]
+      # require "socket"
+      # ip = Socket.ip_address_list
+      #   .detect(&:ipv4_private?)
+      #   .ip_address
+      config.host = "http://manuals-publisher.dev.gov.uk/"
+    end
   end
 end
