a59dda6a2cf5ccab2df6e3e41e0bc78d3313411c
Conor <conor.lowney@digital.cabinet-office.gov.uk>
2023-06-27 15:14:56 +0100

    Add paste-html-to-govspeak to form textareas
    What
    
    Extended manuals _form.html.erb textareas to js-paste-html-to-govspeak class
    Extended sections _form.html.erb textareas to js-paste-html-to-govspeak class
    Extended application.js to have a function to apply pasteHtmlToGovspeak.pasteListener to js-paste-html-to-govspeak class
    Extended package.json to install the NPM package
    
    Why
    
    Implement paste-html-to-govspeak function to textareas on forms so users can paste HTML into them and it convert to govspeak to speed creating and editing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,10 +6,14 @@
 //= require length_counter
 //= require markdown_preview
 //= require toggle_display_with_checked_input
-
+//= require paste-html-to-govspeak
 jQuery(function ($) {
   $('.js-length-counter').each(function () {
     new GOVUK.LengthCounter({ $el: $(this) }) // eslint-disable-line no-new
+  })
+
+  $('.js-paste-html-to-govspeak').each(function () {
+    this.addEventListener('paste', pasteHtmlToGovspeak.pasteListener) // eslint-disable-line no-undef
   })
 
   $('.reorderable-document-list').sortable()
