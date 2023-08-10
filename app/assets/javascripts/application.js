3950e9ba7ad9f5841c08f0a6dfeb5dbb542aebd1
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2023-08-10 08:41:49 +0100

    Add missing require to application.js
    The require for the button's javascript was missing (though functionally
     everything seems to be working OK).

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/skip-link
