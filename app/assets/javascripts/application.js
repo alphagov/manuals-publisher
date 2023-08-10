a080441fb7f43495a82506b56e9b5723509bb829
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2023-08-10 10:24:15 +0100

    List draft sections on publish confirmation page
    Update the manuals publish confirmation page to include a table listing
     the sections that are currently in a draft state and so whose changes
     will be included in the manual's publication. This is to help avoid
     sections getting published that are not ready, by making whoever is
     publishing aware of the changes that will be included (for example,
     when there are multiple people editing sections).
    
    The logic for the "last updated text" was already present on the
     manual's "show" view, so this has been extracted to a helper method and
     reused by the publish confirmation view.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,4 @@
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/skip-link
+//= require govuk_publishing_components/components/table
