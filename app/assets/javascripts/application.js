cd120b30bd3007985e26a8884f503c8805eacf06
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2023-09-06 14:04:53 +0100

    Migrate the reorder sections page to the design system
    Update the page for reordering sections within a manual so that it is
     using the `design_system` layout, and is built using UI components from
     the design system.
    
    The `reorderable_list` component structures the form data differently to
     how the original page did, requiring changes on the server side to
     handle this difference when the data is submitted.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,6 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/layout-header
+//= require govuk_publishing_components/components/reorderable-list
 //= require govuk_publishing_components/components/skip-link
 //= require govuk_publishing_components/components/table
