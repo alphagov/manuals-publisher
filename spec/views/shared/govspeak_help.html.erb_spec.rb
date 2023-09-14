d9087d642f06b0dc030fb94cd8b80b36754ee6a2
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2023-09-14 09:11:18 +0100

    Add collapsible help to sidebar
    Add a section the `govspeak_help` component for "Collapsible" text. This
     is only applicable for sections and so should not be displayed when
     adding/editing a manual.

---
+++ spec/views/shared/govspeak_help.html.erb_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe "shared/_govspeak_help", type: :view do
+  it "renders the help without a collapsible help section by default" do
+    render
+
+    expect(rendered).not_to have_text("Collapsible")
+    expect(rendered).to have_text("Paste and convert to Markdown")
+    expect(rendered).to have_text("Headings")
+    expect(rendered).to have_text("Links")
+    expect(rendered).to have_text("Bullets")
+    expect(rendered).to have_text("Numbered lists")
+    expect(rendered).to have_text("Video links")
+    expect(rendered).to have_text("Legislative list")
+    expect(rendered).to have_text("Tables")
+    expect(rendered).to have_text("Charts")
+    expect(rendered).to have_text("Call to action")
+    expect(rendered).to have_text("Abbreviations and acronyms")
+    expect(rendered).to have_text("Blockquotes")
+    expect(rendered).to have_text("Addresses")
+    expect(rendered).to have_text("Email links")
+    expect(rendered).to have_text("Footnotes")
+  end
+
+  it "renders the help with a collapsible help section" do
+    render partial: "shared/govspeak_help", locals: { show_collapsible_help: true }
+
+    expect(rendered).to have_text("Collapsible")
+    expect(rendered).to have_text("Paste and convert to Markdown")
+    expect(rendered).to have_text("Headings")
+    expect(rendered).to have_text("Links")
+    expect(rendered).to have_text("Bullets")
+    expect(rendered).to have_text("Numbered lists")
+    expect(rendered).to have_text("Video links")
+    expect(rendered).to have_text("Legislative list")
+    expect(rendered).to have_text("Tables")
+    expect(rendered).to have_text("Charts")
+    expect(rendered).to have_text("Call to action")
+    expect(rendered).to have_text("Abbreviations and acronyms")
+    expect(rendered).to have_text("Blockquotes")
+    expect(rendered).to have_text("Addresses")
+    expect(rendered).to have_text("Email links")
+    expect(rendered).to have_text("Footnotes")
+  end
+end
