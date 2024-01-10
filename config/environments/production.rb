bd3e7423b3f1de6f9e86115fe6fdf72eb3931ec0
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2024-01-10 14:11:33 +0000

    Migrate to dart sass
    Previously this project was using LibSass to compile the scss files. LibSass is a deprecated project and Dart Sass is the recommended alternative.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,10 +24,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Rather than use a CSS compressor, use the SASS style to perform compression.
-  config.sass.style = :compressed
-  config.sass.line_comments = false
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
