b079467f775f04b6b10504cf654e3f3c68494530
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-07-11 11:48:48 +0100

    Add rake task for changing org slug for manual records
    The "Change an organisation's slug" instructions, at time of
    writing, ask that we create a 'migration' in Manuals Publisher.
    https://docs.publishing.service.gov.uk/manual/changing-organisation-slug.html#2-update-the-organisation-slug-in-manuals-publisher
    
    But as per previous commit, rails migrations haven't been supported
    in this app since 2020, preferring 'temporary rake tasks'. This
    commit therefore adds a rake task, though the intention is to
    keep it, since we'll need to run it whenever an organisation slug
    is changed.
    
    The slug is only displayed in Manuals Publisher itself (and used
    to determine which manuals the user can see), so no republishing
    is required as part of this task.

---
+++ spec/lib/tasks/reslug_organisation_spec.rb
@@ -0,0 +1,62 @@
+require "spec_helper"
+require "rake"
+
+describe "reslug_organisation" do
+  before :all do
+    Rake::Task.define_task(:environment) # depended on by `reslug_organisation`
+    Rake.application.rake_require("lib/tasks/reslug_organisation", [Rails.root.to_s])
+  end
+  before :each do
+    task.reenable # without this, calling `invoke` does nothing after first test
+    allow($stdout).to receive(:puts) # supress Rake task output
+  end
+  let(:task) { Rake::Task["reslug_organisation"] }
+
+  describe "validation" do
+    let(:error_message) do
+      "Invalid parameters provided to `reslug_organisation` Usage: reslug_organisation[old-slug,new-slug]."
+    end
+
+    it "raises exception if no args provided" do
+      expect { task.invoke }.to raise_error(RuntimeError, error_message)
+    end
+
+    it "raises exception if only one arg provided" do
+      expect { task.invoke("foo") }.to raise_error(RuntimeError, error_message)
+    end
+  end
+
+  it "finds all manual records with the given org slug and replaces its slug" do
+    manual_records_to_update = [
+      FactoryBot.create(:manual_record, organisation_slug: "foo"),
+      FactoryBot.create(:manual_record, organisation_slug: "foo"),
+    ]
+
+    task.invoke("foo", "bar")
+
+    expect(manual_records_to_update.first.reload[:organisation_slug]).to eq("bar")
+    expect(manual_records_to_update.last.reload[:organisation_slug]).to eq("bar")
+  end
+
+  it "outputs useful information about which manual records are having their orgs re-slugged" do
+    FactoryBot.create(:manual_record, organisation_slug: "foo", manual_id: "abc123", slug: "/guidance/test")
+    FactoryBot.create(:manual_record, organisation_slug: "foo", manual_id: "def456", slug: "/guidance/another")
+
+    expected_output = <<~OUTPUT
+      Updating the `organisation_slug` of 2 manual records from 'foo' to 'bar'
+      - Updating ManualRecord abc123 (/guidance/test)
+      - Updating ManualRecord def456 (/guidance/another)
+      Done.
+    OUTPUT
+
+    expect { task.invoke("foo", "bar") }.to output(expected_output).to_stdout
+  end
+
+  it "ignores manual records that have a different org slug" do
+    manual_record_to_ignore = FactoryBot.create(:manual_record, organisation_slug: "leave-me-alone")
+
+    task.invoke("foo", "bar")
+
+    expect(manual_record_to_ignore.reload[:organisation_slug]).to eq("leave-me-alone")
+  end
+end
