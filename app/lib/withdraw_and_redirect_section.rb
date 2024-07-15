92fe2cb01dcdfa4b62b9b94c06f0e1ece6feb490
Minno Dang <mndang@thoughtworks.com>
2024-07-15 15:01:11 +0100

    Fixing rake task so it sends discard draft correctly to Publishing API
    Previously, the raks task for removing sections had a conditional where it only sent discard draft only if the section was a draft and had the discard draft flag. However, we have sections that are currently published which have drafts on Publishing API - we are unable to remove them due to an error "Unable to unpublish edition with draft". Amending this so we can unpublish these sections.
    
    Additionally, a few of the URLs were wrong, and required regenerating.

--- app/lib/withdraw_and_redirect_section.rb
+++ app/lib/withdraw_and_redirect_section.rb
@@ -18,11 +18,7 @@ class WithdrawAndRedirectSection
 
     raise SectionNotPublishedError, section.slug unless section.published?
 
-    if discard_draft && section.draft?
-      Adapters.publishing.unpublish_section(section, redirect:, discard_drafts: true)
-    else
-      Adapters.publishing.unpublish_section(section, redirect:, discard_drafts: false)
-    end
+    Adapters.publishing.unpublish_section(section, redirect:, discard_drafts: discard_draft)
   end
 
 private
