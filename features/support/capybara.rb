baa4a9066c9181838023b262df91e5471b395299
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-04-09 17:42:14 +0100

    WIP

---
+++ features/support/capybara.rb
@@ -0,0 +1,13 @@
+Capybara.default_max_wait_time = 5
+Capybara.automatic_label_click = true
+
+Capybara.register_driver :headless_chrome do |app|
+  chrome_options = GovukTest.headless_chrome_selenium_options
+  chrome_options.add_argument("--no-sandbox")
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: chrome_options,
+    )
+end
